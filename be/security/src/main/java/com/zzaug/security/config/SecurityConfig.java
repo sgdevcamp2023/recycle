b4d41e20e5f72cc55b5ccf9c3b8e8a0b40edb870
belljun3395 <195850@jnu.ac.kr>
2023-12-30 01:38:02 +0900

    feat: 기본 security 모듈 추가
    Related to: #9

---
+++ be/security/src/main/java/com/zzaug/security/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+package com.zzaug.security.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = SecurityConfig.BASE_PACKAGE)
+public class SecurityConfig {
+
+    public static final String BASE_PACKAGE = "com.zzaug.security";
+    public static final String SERVICE_NAME = "recycle";
+    public static final String MODULE_NAME = "security";
+    public static final String BEAN_NAME_PREFIX = "Security";
+    public static final String PROPERTY_PREFIX = SERVICE_NAME + "." + MODULE_NAME;
+}
---
+++ be/security/src/main/java/com/zzaug/security/filter/token/TokenAuthenticationFilter.java
@@ -0,0 +1,48 @@
+package com.zzaug.security.filter.token;
+
+import com.zzaug.security.exception.AccessTokenInvalidException;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+
+@Slf4j
+public class TokenAuthenticationFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+	private static final Pattern PATTERN_AUTHORIZATION_HEADER = Pattern.compile("^[Bb]earer (.*)$");
+
+	@Override
+	protected Object getPreAuthenticatedPrincipal(HttpServletRequest request) {
+		return resolveAccessToken(request);
+	}
+
+	@Override
+	protected Object getPreAuthenticatedCredentials(HttpServletRequest request) {
+		return resolveAccessToken(request);
+	}
+
+	private String resolveAccessToken(HttpServletRequest request) {
+		String authorization = request.getHeader("Authorization");
+		if (Objects.isNull(authorization)) {
+			AccessTokenInvalidException exception =
+					getAccessTokenInvalidException("Authorization header is missing");
+			throw exception;
+		}
+		Matcher matcher = PATTERN_AUTHORIZATION_HEADER.matcher(authorization);
+		if (!matcher.matches()) {
+			AccessTokenInvalidException exception =
+					getAccessTokenInvalidException("Authorization header is not a Bearer token");
+			throw exception;
+		}
+		return matcher.group(1);
+	}
+
+	private AccessTokenInvalidException getAccessTokenInvalidException(String message) {
+		AccessTokenInvalidException exception = new AccessTokenInvalidException(message);
+		log.warn(exception.getMessage());
+		return exception;
+	}
+}
