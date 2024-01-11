a5d34fd279582457f7f4efcf00e2f3743d5a605a
belljun3395 <195850@jnu.ac.kr>
2024-01-11 14:27:43 +0900

    feat: 구현 MemberController
    - 기획에서 정의된 API 명세에 따라 구현
    - 미리 정의한 객체를 통해 고정된 값(목객체)를 반환하도록 구현
    
    Related to: #54

--- be/member-api/src/main/java/com/zzaug/member/web/controller/v1/member/MemberController.java
+++ be/member-api/src/main/java/com/zzaug/member/web/controller/v1/member/MemberController.java
@@ -1,23 +1,38 @@
 package com.zzaug.member.web.controller.v1.member;
 
-import com.zzaug.member.domain.dto.member.MemberResponse;
-import com.zzaug.member.domain.dto.member.MemberUseCaseRequest;
-import com.zzaug.member.domain.usecase.member.MemberUseCase;
+import com.zzaug.member.domain.dto.member.DeleteMemberUseCaseRequest;
+import com.zzaug.member.domain.dto.member.GetMemberUseCaseRequest;
+import com.zzaug.member.domain.dto.member.GetMemberUseCaseResponse;
+import com.zzaug.member.domain.dto.member.LogOutUseCaseRequest;
+import com.zzaug.member.domain.dto.member.LoginUseCaseRequest;
+import com.zzaug.member.domain.dto.member.MemberAuthToken;
+import com.zzaug.member.domain.dto.member.PostMemberUseCaseRequest;
+import com.zzaug.member.domain.dto.member.SearchMemberUseCaseRequest;
+import com.zzaug.member.domain.dto.member.SearchMemberUseCaseResponse;
+import com.zzaug.member.domain.dto.member.UpdateMemberUseCaseRequest;
 import com.zzaug.member.support.ApiResponse;
 import com.zzaug.member.support.ApiResponseGenerator;
-import com.zzaug.member.web.dto.member.MemberRequest;
-import com.zzaug.member.web.support.usecase.MemberUseCaseRequestConverter;
-import com.zzaug.security.authentication.authority.Roles;
+import com.zzaug.member.support.CookieGenerator;
+import com.zzaug.member.support.CookieSameSite;
+import com.zzaug.member.support.MessageCode;
+import com.zzaug.member.web.dto.member.LoginRequest;
+import com.zzaug.member.web.dto.member.MemberSaveRequest;
+import com.zzaug.member.web.dto.member.MemberUpdateRequest;
 import com.zzaug.security.authentication.token.TokenUserDetails;
-import com.zzaug.security.token.AuthToken;
-import com.zzaug.security.token.TokenGenerator;
-import java.util.List;
+import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -25,21 +40,104 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class MemberController {
 
-	private final MemberUseCase MemberUseCase;
-	private final TokenGenerator tokenGenerator;
+	private final CookieGenerator cookieGenerator;
 
 	@PostMapping()
-	public ApiResponse<ApiResponse.SuccessBody<MemberResponse>> member(
-			@AuthenticationPrincipal TokenUserDetails userDetails, MemberRequest request) {
-		MemberUseCaseRequest useCaseRequest = MemberUseCaseRequestConverter.from(request);
-		//		MemberResponse res = MemberUseCase.execute(useCaseRequest);
-		MemberResponse res = MemberResponse.builder().name("name").build();
-		return ApiResponseGenerator.success(res, HttpStatus.OK);
+	public ApiResponse<ApiResponse.Success> save(@RequestBody MemberSaveRequest request) {
+		PostMemberUseCaseRequest useCaseRequest =
+				PostMemberUseCaseRequest.builder()
+						.certification(request.getCertification())
+						.password(request.getPassword())
+						.build();
+		return ApiResponseGenerator.success(HttpStatus.CREATED, MessageCode.RESOURCE_CREATED);
 	}
 
-	@GetMapping("token")
-	public ApiResponse<ApiResponse.SuccessBody<AuthToken>> token() {
-		AuthToken res = tokenGenerator.generateAuthToken(1L, List.of(Roles.ROLE_USER));
-		return ApiResponseGenerator.success(res, HttpStatus.OK);
+	@PutMapping()
+	public ApiResponse<ApiResponse.Success> update(
+			@AuthenticationPrincipal TokenUserDetails userDetails,
+			@RequestBody MemberUpdateRequest request) {
+		//		Long memberId = Long.valueOf(userDetails.getId());
+		Long memberId = 1L;
+		UpdateMemberUseCaseRequest useCaseRequest =
+				UpdateMemberUseCaseRequest.builder()
+						.memberId(memberId)
+						.certification(request.getCertification())
+						.password(request.getPassword())
+						.build();
+		return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.RESOURCE_MODIFIED);
+	}
+
+	@DeleteMapping()
+	public ApiResponse<ApiResponse.Success> delete(
+			@AuthenticationPrincipal TokenUserDetails userDetails) {
+		//		Long memberId = Long.valueOf(userDetails.getId());
+		Long memberId = 1L;
+		DeleteMemberUseCaseRequest useCaseRequest =
+				DeleteMemberUseCaseRequest.builder().memberId(memberId).build();
+		return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.RESOURCE_DELETED);
+	}
+
+	@PostMapping("/login")
+	public ApiResponse<ApiResponse.SuccessBody<MemberAuthToken>> login(
+			@RequestBody LoginRequest request, HttpServletResponse httpServletResponse) {
+		LoginUseCaseRequest useCaseRequest =
+				LoginUseCaseRequest.builder()
+						.certification(request.getCertification())
+						.password(request.getPassword())
+						.build();
+		MemberAuthToken response =
+				MemberAuthToken.builder().accessToken("accessToken").refreshToken("refreshToken").build();
+		ResponseCookie cookie =
+				cookieGenerator.createCookie(CookieSameSite.NONE, "refresh_token", "refreshToken");
+		httpServletResponse.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+		return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.SUCCESS);
+	}
+
+	@PostMapping("/logout")
+	public ApiResponse<ApiResponse.Success> logout(
+			@AuthenticationPrincipal TokenUserDetails userDetails) {
+		//		Long memberId = Long.valueOf(userDetails.getId());
+		Long memberId = 1L;
+		LogOutUseCaseRequest useCaseRequest = LogOutUseCaseRequest.builder().memberId(memberId).build();
+		return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.SUCCESS);
+	}
+
+	@GetMapping("/{id}")
+	public ApiResponse<ApiResponse.SuccessBody<GetMemberUseCaseResponse>> read(
+			@AuthenticationPrincipal TokenUserDetails userDetails, @PathVariable Long id) {
+		//		Long memberId = Long.valueOf(userDetails.getId());
+		Long memberId = 1L;
+		GetMemberUseCaseRequest useCaseRequest =
+				GetMemberUseCaseRequest.builder().memberId(memberId).queryMemberId(id).build();
+		GetMemberUseCaseResponse response =
+				GetMemberUseCaseResponse.builder().id(1L).email("email").github("github").build();
+		return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.SUCCESS);
+	}
+
+	@GetMapping()
+	public ApiResponse<ApiResponse.SuccessBody<SearchMemberUseCaseResponse>> search(
+			@AuthenticationPrincipal TokenUserDetails userDetails,
+			@RequestParam(value = "certification", required = false) String certification) {
+		//		Long memberId = Long.valueOf(userDetails.getId());
+		Long memberId = 1L;
+		SearchMemberUseCaseRequest useCaseRequest =
+				SearchMemberUseCaseRequest.builder()
+						.memberId(memberId)
+						.certification(certification)
+						.build();
+		SearchMemberUseCaseResponse response =
+				SearchMemberUseCaseResponse.builder().id(1L).email("email").github("github").build();
+		return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.SUCCESS);
+	}
+
+	@PostMapping("/token")
+	public ApiResponse<ApiResponse.SuccessBody<MemberAuthToken>> token(
+			@AuthenticationPrincipal TokenUserDetails userDetails) {
+		//		Long memberId = Long.valueOf(userDetails.getId());
+		Long memberId = 1L;
+		LogOutUseCaseRequest useCaseRequest = LogOutUseCaseRequest.builder().memberId(memberId).build();
+		MemberAuthToken response =
+				MemberAuthToken.builder().accessToken("accessToken").refreshToken("refreshToken").build();
+		return ApiResponseGenerator.success(response, HttpStatus.OK, MessageCode.SUCCESS);
 	}
 }
