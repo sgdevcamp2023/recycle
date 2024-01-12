13fe965a401c43cd14a17667a2aa87c4d0e45fd8
sso9594 <sso9594@naver.com>
2024-01-12 10:40:51 +0900

    refactor: 파라미터 수정

--- be/review-api/src/main/java/com/zzaug/review/web/controller/v1/review/ReviewController.java
+++ be/review-api/src/main/java/com/zzaug/review/web/controller/v1/review/ReviewController.java
@@ -34,7 +34,7 @@ public class ReviewController {
 	@PostMapping("/questions/{question_id}/reviews")
 	public ApiResponse<ApiResponse.SuccessBody<Void>> createReview(
 			@AuthenticationPrincipal TokenUserDetails userDetails,
-			@RequestParam Long question_id,
+			@PathVariable Long question_id,
 			ReviewRequest request) {
 
 		return ApiResponseGenerator.success(HttpStatus.OK);
@@ -43,8 +43,8 @@ public class ReviewController {
 	@PostMapping("/questions/{question_id}/reviews/temp")
 	public ApiResponse<ApiResponse.SuccessBody<Void>> createTempReview(
 			@AuthenticationPrincipal TokenUserDetails userDetails,
-			@RequestParam Long question_id,
-			ReviewTempRequest request) {
+			@PathVariable Long question_id,
+			@RequestBody ReviewTempRequest request) {
 
 		return ApiResponseGenerator.success(HttpStatus.OK);
 	}
@@ -72,7 +72,7 @@ public class ReviewController {
 
 	@GetMapping("/questions/{question_id}/reviews")
 	public ApiResponse<ApiResponse.SuccessBody<List<ReviewResponse>>> viewQuestionReviewList(
-			@AuthenticationPrincipal TokenUserDetails userDetails, @RequestParam Long question_id) {
+			@AuthenticationPrincipal TokenUserDetails userDetails, @PathVariable Long question_id) {
 
 		List<ReviewResponse> responses = new ArrayList<>();
 		ReviewResponse res =
@@ -118,7 +118,8 @@ public class ReviewController {
 	public ApiResponse<ApiResponse.SuccessBody<Void>> editReview(
 			@AuthenticationPrincipal TokenUserDetails userDetails,
 			@PathVariable Long question_id,
-			@PathVariable Long review_id) {
+			@PathVariable Long review_id,
+			@RequestBody ReviewRequest request) {
 
 		return ApiResponseGenerator.success(HttpStatus.OK);
 	}
