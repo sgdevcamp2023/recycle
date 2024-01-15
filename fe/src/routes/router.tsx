ffd5b858869b16a5055f664c4fa4c709189a71f6
Klomachenko <leekyumin0901@naver.com>
2024-01-15 19:47:58 +0900

    feat : testPage Router 추가
    Related to #65

--- fe/src/routes/router.tsx
+++ fe/src/routes/router.tsx
@@ -1,16 +1,18 @@
-import Login from "@page/Login";
-import NotFound from "@page/NotFound";
-import SignUp from "@page/SignUp";
-import Index from "@page/Index";
-import { Route, Routes } from "react-router-dom";
+import Login from '@page/Login';
+import NotFound from '@page/NotFound';
+import SignUp from '@page/SignUp';
+import Index from '@page/Index';
+import { Route, Routes } from 'react-router-dom';
+import TestPage from '@page/TestPage';
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/*" element={<NotFound />} />
+      <Route path='/' element={<Index />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/*' element={<NotFound />} />
+      <Route path='/test' element={<TestPage />} />
     </Routes>
   );
 };
