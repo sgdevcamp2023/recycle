34b35120e401180df801fc6847ab7e3ecb7ea1df
Klomachenko <leekyumin0901@naver.com>
2024-01-15 11:30:09 +0900

    feat: CommonButton 구현
    Related to #65

--- fe/src/main.tsx
+++ fe/src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import theme from "@styles/theme.ts";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from '@styles/theme.ts';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <ThemeProvider theme={theme}>
       <React.StrictMode>
--- fe/src/page/TestPage.tsx
+++ fe/src/page/TestPage.tsx
@@ -1,7 +1,12 @@
+import CommonButton from '@components/atom/Button/CommonButton';
+import ReviewButton from '@components/atom/Button/ReviewButton';
+
 const TestPage = () => {
   return (
     <>
-      <>test</>
+      <>테스트페이지임여</>
+      <CommonButton></CommonButton>
+      <ReviewButton></ReviewButton>
     </>
   );
 };
