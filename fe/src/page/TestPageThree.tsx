699886b8e696f50eeb596e08f69f43b9bd39ca1f
Klomachenko <leekyumin0901@naver.com>
2024-01-17 14:32:56 +0900

    style: TestPageThree 스타일 수정
    Description: 내부 모달들을 더 잘 볼 수 있도록 가운데 정렬 div를
    추가하였습니다.

--- fe/src/page/TestPageThree.tsx
+++ fe/src/page/TestPageThree.tsx
@@ -1,11 +1,20 @@
 import AskLoginModal from '@components/block/modal/AskLoginModal';
+import styled from 'styled-components';
+
+const CenterModalBox = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
 const TestPageThree = () => {
   return (
     <>
-      <div>
+      <CenterModalBox>
         <AskLoginModal />
-      </div>
+      </CenterModalBox>
     </>
   );
 };
