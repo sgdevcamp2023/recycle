a422376d40e67e9fbccb129b26c7385cc24de29e
Klomachenko <leekyumin0901@naver.com>
2024-01-17 17:09:48 +0900

    feat: ShowReviewerModal 구현
    Related to ShowReviewerModal 구현 #116

--- fe/src/page/TestPageThree.tsx
+++ fe/src/page/TestPageThree.tsx
@@ -1,5 +1,6 @@
 import AskLoginModal from '@components/block/modal/AskLoginModal';
 import SaveModal from '@components/block/modal/SaveModal';
+import ShowReviewerModal from '@components/block/modal/ShowReviewerModal';
 import styled from 'styled-components';
 
 const CenterModalBox = styled.div`
@@ -15,7 +16,8 @@ const TestPageThree = () => {
     <>
       <CenterModalBox>
         {/* <AskLoginModal /> */}
-        <SaveModal />
+        {/* <SaveModal /> */}
+        <ShowReviewerModal />
       </CenterModalBox>
     </>
   );
