004dc341e834bd3051707717e0cfa55027f79e4d
belljun3395 <195850@jnu.ac.kr>
2024-01-20 17:34:19 +0900

    feat: 수정 ZRMQConnectionConfig 요소
    기존 상수로 선언한 요소 RabbitProperties 활용하는 방법으로 수정
    
    파라미터가 아닌 메서드 활용하는 방식으로 수정
    
    Related to: #59

--- be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/ZRMQConnectionConfig.java
+++ be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/ZRMQConnectionConfig.java
@@ -1,14 +1,13 @@
 package com.zzaug.rabbitmq.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,30 +15,20 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class ZRMQConnectionConfig {
 
-	private final int MAX_TRY_COUNT = 3;
-	private final int INITIAL_INTERVAL = 3000;
-	private final int MULTIPLIER = 3;
-	private final int MAX_INTERVAL = 10000;
-
-	@Value("${spring.rabbitmq.host}")
-	private String host;
-
-	@Value("${spring.rabbitmq.username}")
-	private String username;
-
-	@Value("${spring.rabbitmq.password}")
-	private String password;
-
-	@Value("${spring.rabbitmq.port}")
-	private int port;
+	@Bean
+	@ConfigurationProperties(prefix = "spring.rabbitmq")
+	RabbitProperties rabbitMQProperties() {
+		return new RabbitProperties();
+	}
 
 	@Bean
 	ConnectionFactory connectionFactory() {
+		RabbitProperties properties = rabbitMQProperties();
 		CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
-		connectionFactory.setHost(host);
-		connectionFactory.setPort(port);
-		connectionFactory.setUsername(username);
-		connectionFactory.setPassword(password);
+		connectionFactory.setHost(properties.getHost());
+		connectionFactory.setPort(properties.getPort());
+		connectionFactory.setUsername(properties.getUsername());
+		connectionFactory.setPassword(properties.getPassword());
 		return connectionFactory;
 	}
 
@@ -49,31 +38,13 @@ public class ZRMQConnectionConfig {
 	}
 
 	@Bean
-	public RabbitTemplate amqpTemplate(
-			ConnectionFactory connectionFactory, MessageConverter queueMessageConverter) {
+	public RabbitTemplate amqpTemplate() {
 		RabbitTemplate rabbitTemplate = new RabbitTemplate();
-		rabbitTemplate.setConnectionFactory(connectionFactory);
+		rabbitTemplate.setConnectionFactory(connectionFactory());
 		rabbitTemplate.setMandatory(true);
 		rabbitTemplate.setChannelTransacted(true);
 		rabbitTemplate.setReplyTimeout(60000);
-		rabbitTemplate.setMessageConverter(queueMessageConverter);
+		rabbitTemplate.setMessageConverter(messageConverter());
 		return rabbitTemplate;
-	}
-
-	@Bean
-	SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
-			RabbitTemplate rabbitTemplate) {
-		final SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-		factory.setConnectionFactory(connectionFactory());
-		factory.setDefaultRequeueRejected(false);
-		factory.setMessageConverter(messageConverter());
-		factory.setChannelTransacted(true);
-		factory.setAdviceChain(
-				RetryInterceptorBuilder.stateless()
-						.maxAttempts(MAX_TRY_COUNT)
-						.recoverer(new ZRMQExceptionHandler(rabbitTemplate))
-						.backOffOptions(INITIAL_INTERVAL, MULTIPLIER, MAX_INTERVAL)
-						.build());
-		return factory;
 	}
 }
