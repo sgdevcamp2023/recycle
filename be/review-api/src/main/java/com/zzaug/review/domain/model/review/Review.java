ad84980fb48d652104a00c7dae4d02bab2264efa
sso9594 <sso9594@naver.com>
2024-01-20 21:47:41 +0900

    feat: Review 도메인 모델 추가
    Related to: #112

--- be/review-api/src/main/java/com/zzaug/review/domain/model/review/Review.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/model/review/Review.java
@@ -1,10 +1,15 @@
 package com.zzaug.review.domain.model.review;
 
+import com.zzaug.review.entity.review.ReviewPoint;
+import com.zzaug.review.entity.review.ReviewType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import javax.persistence.Embedded;
+import java.time.LocalDateTime;
 
 @Getter
 @ToString
@@ -13,5 +18,26 @@ import lombok.ToString;
 @Builder(toBuilder = true)
 public class Review {
 
-	private String name;
+	private Long reviewId;
+
+	private Long questionId;
+
+	private String content;
+
+	private String author;
+
+	private Long authorId;
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+
+	@Embedded
+	private ReviewPoint startPoint;
+
+	@Embedded
+	private ReviewPoint endPoint;
+
+	private ReviewType tag;
+
 }
---
+++ be/review-api/src/main/java/com/zzaug/review/domain/model/review/ReviewTemp.java
@@ -0,0 +1,29 @@
+package com.zzaug.review.domain.model.review;
+
+import com.zzaug.review.entity.review.ReviewPoint;
+import com.zzaug.review.entity.review.ReviewType;
+import lombok.*;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class ReviewTemp {
+    private String tId;
+    private Long questionId;
+    private String content;
+    private String author;
+    private Long authorId;
+    private LocalDateTime createdAt;
+    private ReviewPoint startPoint;
+    private ReviewPoint endPoint;
+    private ReviewType tag;
+}
