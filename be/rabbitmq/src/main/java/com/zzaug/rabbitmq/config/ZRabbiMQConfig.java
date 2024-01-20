2b739c0b4f48410686f54049e8ec33d69b5bec5b
belljun3395 <195850@jnu.ac.kr>
2024-01-21 02:57:37 +0900

    refactor: 수정 RabbitAutoConfiguration 실행 제외
    Related to: #59

--- be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/ZRabbiMQConfig.java
+++ be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/ZRabbiMQConfig.java
@@ -1,12 +1,15 @@
 package com.zzaug.rabbitmq.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = ZRabbiMQConfig.BASE_PACKAGE)
+@EnableAutoConfiguration(exclude = RabbitAutoConfiguration.class)
 public class ZRabbiMQConfig {
 
 	public static final String BASE_PACKAGE = "com.zzaug.rabbitmq";
