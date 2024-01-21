bd4dc049fefd634c7f19665cebb9215ef8ff74a5
krokerdile <crazykroker@gmail.com>
2024-01-21 15:13:26 +0900

    feat: Common 추가 Related to 프론트 전체 라우팅 작업 진행 #136

---
+++ fe/src/page/Common.tsx
@@ -0,0 +1,9 @@
+const Common = () => {
+  return (
+    <>
+      <h1> Common</h1>
+    </>
+  );
+};
+
+export default Common;
--- fe/src/routes/router.tsx
+++ fe/src/routes/router.tsx
@@ -1,7 +1,7 @@
 import Login from "@page/Login";
 import NotFound from "@page/NotFound";
 import SignUp from "@page/SignUp";
-import Index from "@page/Index";
+import Common from "@page/Common";
 import { Route, Routes } from "react-router-dom";
 import TestPage from "@page/TestPage";
 import TestPageTwo from "@page/TestPageTwo";
@@ -11,10 +11,15 @@ import GridTemplate from "@components/layout/GridTemplate";
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Index />} />
+      {/* 메인 페이지 */}
+      <Route element={<GridTemplate />}>
+        <Route path="/" element={<Common />} />
+      </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/*" element={<NotFound />} />
+
+      {/* 테스트 페이지 */}
       <Route path="/test" element={<TestPage />} />
       <Route path="/test2" element={<TestPageTwo />} />
       <Route path="/test3" element={<TestPageThree />} />
