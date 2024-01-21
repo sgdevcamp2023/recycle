ec0d16cad2f4160979c3b4aa47487b18efb531e6
belljun3395 <195850@jnu.ac.kr>
2024-01-21 13:18:29 +0900

    feat: 추가 RabbitMQ 설정
    RabbitAutoConfiguration를 참고하여 부족한 부분을 추가하였습니다.
    
    CachingConnectionFactoryConfigurer,
    RabbitMessagingTemplate
    
    Related to: #59

--- be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/ZRMQConnectionConfig.java
+++ be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/ZRMQConnectionConfig.java
@@ -4,11 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionNameStrategy;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitMessagingTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.amqp.CachingConnectionFactoryConfigurer;
 import org.springframework.boot.autoconfigure.amqp.RabbitProperties;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,23 +40,35 @@ public class ZRMQConnectionConfig {
 	}
 
 	@Bean
-	MessageConverter messageConverter() {
-		return new Jackson2JsonMessageConverter();
+	@ConditionalOnMissingBean
+	CachingConnectionFactoryConfigurer rabbitConnectionFactoryConfigurer(
+			RabbitProperties rabbitProperties,
+			ObjectProvider<ConnectionNameStrategy> connectionNameStrategy) {
+		CachingConnectionFactoryConfigurer configurer =
+				new CachingConnectionFactoryConfigurer(rabbitProperties);
+		configurer.setConnectionNameStrategy(connectionNameStrategy.getIfUnique());
+		return configurer;
 	}
 
 	@Bean
-	public RabbitTemplate amqpTemplate() {
+	public RabbitTemplate rabbitTemplate(ObjectProvider<MessageConverter> messageConverter) {
 		RabbitTemplate rabbitTemplate = new RabbitTemplate();
 		rabbitTemplate.setConnectionFactory(connectionFactory());
 		rabbitTemplate.setMandatory(true);
 		rabbitTemplate.setChannelTransacted(true);
 		rabbitTemplate.setReplyTimeout(60000);
-		rabbitTemplate.setMessageConverter(messageConverter());
+		rabbitTemplate.setMessageConverter(messageConverter.getIfUnique());
 		return rabbitTemplate;
 	}
 
 	@Bean
 	public AmqpAdmin amqpAdmin(ConnectionFactory connectionFactory) {
 		return new RabbitAdmin(connectionFactory);
+	}
+
+	@Bean
+	@ConditionalOnSingleCandidate(RabbitTemplate.class)
+	public RabbitMessagingTemplate rabbitMessagingTemplate(RabbitTemplate rabbitTemplate) {
+		return new RabbitMessagingTemplate(rabbitTemplate);
 	}
 }
