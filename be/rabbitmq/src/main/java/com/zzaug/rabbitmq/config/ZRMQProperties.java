60cc6c56c2e4b425e9d813d418517a39fb6d3091
belljun3395 <195850@jnu.ac.kr>
2024-01-21 22:28:52 +0900

    feat: 추가 ZRMQProperties#DEAD_LETTER_KEY_NAME
    Related to: #59

--- be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/ZRMQProperties.java
+++ be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/ZRMQProperties.java
@@ -3,6 +3,7 @@ package com.zzaug.rabbitmq.config;
 public interface ZRMQProperties {
 
 	String DEAD_LETTER_EXCHANGE_NAME = "dead-letter-exchange";
+	String DEAD_LETTER_KEY_NAME = "dead-letter-queue";
 	String DEAD_LETTER_QUEUE_NAME = "dead-letter-queue";
 
 	String MEMBER_KEY_NAME = "zzuag.member";
