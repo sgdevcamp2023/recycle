0adbef5d81b42efe877fded06b1cea77e0d03229
krokerdile <crazykroker@gmail.com>
2024-01-22 15:03:40 +0900

    feat: DefaultTab Text 추가 Related to 질문, 리뷰, 리뷰요청, 설정 페이지 구성 #135

--- fe/src/components/block/navbar/DefaultTab.tsx
+++ fe/src/components/block/navbar/DefaultTab.tsx
@@ -1,3 +1,4 @@
+import Text from "@components/atom/Text";
 import useTabStore, { DefaultTabType } from "@store/useTabStore";
 import { useState } from "react";
 import styled from "styled-components";
@@ -27,7 +28,9 @@ const DefaultTab = ({ items }: DefaultTabProps) => {
       <NavbarContainer>
         {Object.entries(items).map(([key, label]) => (
           <NavItem key={key} isActive={key === activeKey} onClick={() => handleItemClick({ key, label })}>
-            {key}
+            <Text fontSize="lg" fontWeight="bold">
+              {key}
+            </Text>
           </NavItem>
         ))}
       </NavbarContainer>
@@ -39,22 +42,23 @@ export default DefaultTab;
 
 const TabWrapper = styled.div`
   width: auto;
-  height: 4rem;
+  height: 3rem;
 `;
 
 const NavbarContainer = styled.div`
   display: flex;
   background-color: white;
-  padding: 10px;
+  gap: 0.5rem;
 `;
 
 const NavItem = styled.div<{ isActive: boolean }>`
   padding: 8px 16px;
   cursor: pointer;
-  color: ${({ isActive }) => (isActive ? "#000" : "#888")};
+  color: ${({ isActive }) => (isActive ? "white" : "#888")};
+  background-color: ${({ theme, isActive }) => (isActive ? theme.backgroundColor.green200 : theme.backgroundColor.white)};
   border-radius: 8px;
   &:hover {
-    background-color: ${({ theme }) => theme.backgroundColor.green300};
+    background-color: ${({ theme }) => theme.backgroundColor.green200};
     color: white;
   }
 `;
