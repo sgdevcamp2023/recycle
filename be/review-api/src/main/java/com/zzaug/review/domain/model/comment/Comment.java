cb0f42b6d58970b4fcd913267f38bd6a6191f520
sso9594 <sso9594@naver.com>
2024-01-24 12:24:16 +0900

    feat: Comment 모델 클래스 추가
    Related to: #134

---
+++ be/review-api/src/main/java/com/zzaug/review/domain/model/comment/Comment.java
@@ -0,0 +1,21 @@
+package com.zzaug.review.domain.model.comment;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class Comment {
+    private Long commentId;
+    private Long questionId;
+    private String content;
+    private String author;
+    private Long authorId;
+    private Long parentId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
