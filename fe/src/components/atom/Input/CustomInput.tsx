2dc86d6546f9baf9cd0d4145f177116023b8ff7d
Klomachenko <leekyumin0901@naver.com>
2024-01-24 15:29:19 +0900

    feat: CustomInput을 통한 기존 Input 리팩토링 Description: CustomInput을 통해 기존 Input인 DefaultInput, CommonInp;ut을 삭제하고, 대체하였습니다. Related to Input 리팩터링 #177

---
+++ fe/src/components/atom/Input/CustomInput.tsx
@@ -0,0 +1,114 @@
+import { BackgroundColorType, ColorType, FontSizeType } from '@styles/theme';
+import { RefObject, forwardRef } from 'react';
+import styled from 'styled-components';
+
+interface CustomInputProps {
+  type: string; // password, normal
+  width?: number | string;
+  height?: number | string;
+  maxLength?: number;
+  initValue?: string;
+  placeholder?: string;
+  fontSize?: FontSizeType;
+  color?: ColorType;
+  backgroundColor?: BackgroundColorType;
+  padding?: number | string;
+}
+
+const CustomInput = forwardRef<HTMLInputElement, CustomInputProps>(
+  (
+    {
+      type = 'text',
+      initValue = '',
+      maxLength = 524288,
+      width = '100%',
+      height = '100%',
+      placeholder = '',
+      fontSize = 'base',
+      color = 'black',
+      backgroundColor = 'white',
+      padding = '0.5rem',
+    },
+    ref
+  ) => {
+    if (initValue && ref) {
+      (ref as RefObject<HTMLInputElement>)!.current!.value = initValue;
+    }
+
+    return (
+      <StyledInput
+        ref={ref}
+        type={type}
+        maxLength={maxLength}
+        width={width}
+        height={height}
+        placeholder={placeholder}
+        fontSize={fontSize}
+        color={color}
+        backgroundColor={backgroundColor}
+        padding={padding}
+      />
+    );
+  }
+);
+
+const StyledInput = styled.input<
+  Pick<
+    CustomInputProps,
+    'padding' | 'width' | 'height' | 'fontSize' | 'color' | 'backgroundColor'
+  >
+>`
+  width: ${({ width }) => (width === '100%' ? 'calc(100%)' : `${width}rem`)};
+  height: ${({ height }) => (height === '100%' ? '100%' : `${height}rem`)};
+  padding: ${({ padding }) => (padding === '0.5rem' ? '0.5rem' : `${padding}`)};
+  border: 0.0625rem solid #aaa;
+  border-radius: 0.25rem;
+  color: ${({ theme, color }) =>
+    color ? theme.color[color] : theme.color.black};
+  font-weight: 500;
+  font-size: ${({ theme, fontSize }) =>
+    fontSize ? theme.fontSize[fontSize] : theme.fontSize.base};
+  background-color: ${({ theme, backgroundColor }) =>
+    backgroundColor
+      ? theme.backgroundColor[backgroundColor]
+      : theme.backgroundcolor.inherit};
+
+  ::placeholder {
+    color: ${({ theme }) => theme.color.grey};
+  }
+`;
+
+export default CustomInput;
+
+// const CustomInput = ({
+//   width = '22rem',
+//   height = '3rem',
+//   placeholder = '',
+//   fontSize = '1rem',
+//   fontWeight = 'normal',
+//   color = 'black',
+//   backgroundColor = 'white',
+//   value,
+//   borderRadius = '0.25rem',
+//   border = '0.063rem solid #AAAAAA',
+//   padding = '',
+//   marginBottom = '',
+// }: CustomInputProps) => {
+//   return (
+//     <StyledInput
+//       type='text'
+//       value={value}
+//       width={width}
+//       height={height}
+//       placeholder={placeholder}
+//       fontSize={fontSize}
+//       fontWeight={fontWeight}
+//       color={color}
+//       backgroundColor={backgroundColor}
+//       borderRadius={borderRadius}
+//       border={border}
+//       padding={padding}
+//       marginBottom={marginBottom}
+//     />
+//   );
+// };
--- fe/src/page/TestPageThree.tsx
+++ fe/src/page/TestPageThree.tsx
@@ -1,35 +1,10 @@
-import { Button } from '@/stories/Button';
-import DefaultButton from '@components/atom/Button/DefaultButton';
-import EmailEnrollButton from '@components/atom/Button/EmailEnrollButton';
-import ExperienceButton from '@components/atom/Button/ExperienceButton';
-import GreyButton from '@components/atom/Button/GreyButton';
-import InviteButton from '@components/atom/Button/InviteButton';
-import LinkToGitButton from '@components/atom/Button/LinkToGitButton';
-import ModifyButton from '@components/atom/Button/ModifyButton';
-import RegisterButton from '@components/atom/Button/RegisterButton';
-import ReverseButton from '@components/atom/Button/ReverseButton';
-import SaveReviewButton from '@components/atom/Button/SaveReviewButton';
-import LineCommentView from '@components/atom/Comment/LineCommentView';
-import LineCommentWrite from '@components/atom/Comment/LineCommentWrite';
-import BelowCommentButton from '@components/atom/button/BelowCommentButton';
-import DoubleCheckButton from '@components/atom/button/DoubleCheckButton';
-import ReplyOfCommentButton from '@components/atom/button/ReplyOfCommentButton';
-import ReturnButton from '@components/atom/button/ReturnButton';
-import ReviewButton from '@components/atom/button/ReviewButton';
-import TemporaryStorageButton from '@components/atom/button/TemporaryStorageButton';
-import AskLoginModal from '@components/block/modal/AskLoginModal';
-import BackgroundModal from '@components/block/modal/BackgroundModal';
-import BelowCommentModal from '@components/block/modal/BelowCommentModal';
-import BelowCommentReplyModal from '@components/block/modal/BelowCommentReplyModal';
-import LockModal from '@components/block/modal/LockModal';
+import CommonInput from '@components/atom/Input/CommonInput';
+import CustomInput from '@components/atom/Input/CustomInput';
+import DefaultInput from '@components/atom/Input/CustomInput';
 import LoginModal from '@components/block/modal/LoginModal';
 import RegisterEmailModal from '@components/block/modal/RegisterEmailModal';
-import ReviewToGitModal from '@components/block/modal/ReviewToGitModal';
-import SaveModal from '@components/block/modal/SaveModal';
-import SendEmailModal from '@components/block/modal/SendEmailModal';
-import SharingModal from '@components/block/modal/SharingModal';
-import ShowReviewerModal from '@components/block/modal/ShowReviewerModal';
 import SignInModal from '@components/block/modal/SignInModal';
+
 import styled from 'styled-components';
 
 const CenterModalBox = styled.div`
@@ -45,20 +20,10 @@ const TestPageThree = () => {
   return (
     <>
       <CenterModalBox>
-        {/* <AskLoginModal /> */}
-        {/* <SignInModal /> */}
-        {/* <SaveModal /> */}
-        {/* <ShowReviewerModal /> */}
-        {/* <SharingModal /> */}
-        {/* <LockModal /> */}
+        {/* <CustomInput width={22} height={3} type='email' /> */}
+        {/* <LoginModal /> */}
+        <SignInModal />
         {/* <RegisterEmailModal /> */}
-        {/* <ReviewToGitModal /> */}
-        {/* <SendEmailModal /> */}
-        {/* <BelowCommentModal /> */}
-        {/* <BelowCommentReplyModal /> */}
-        <LineCommentView />
-        <ModifyButton />
-        <SaveModal />
       </CenterModalBox>
     </>
   );
