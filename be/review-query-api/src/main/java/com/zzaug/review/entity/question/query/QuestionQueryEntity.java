661d49745f807990d4a43a136a73bc213f2cd61e
sso9594 <sso9594@naver.com>
2024-01-24 23:36:34 +0900

    feat: Question 도메인의 Read모델 entity 클래스 추가
    Related to: #179

---
+++ be/review-query-api/src/main/java/com/zzaug/review/entity/question/query/QuestionQueryEntity.java
@@ -0,0 +1,46 @@
+package com.zzaug.review.entity.question.query;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.*;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@Document(indexName = "question")
+public class QuestionQueryEntity {
+
+	@Id
+	private Long questionId;
+
+	@Field(type = FieldType.Text)
+	private String content;
+
+	@Field(type = FieldType.Text)
+	private String author;
+
+	private Long authorId;
+
+	private int reviewCnt;
+
+	@Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd HH:mm:ss || yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC+9")
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	private LocalDateTime createdAt;
+
+	@Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd HH:mm:ss || yyyy-MM-dd")
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC+9")
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	private LocalDateTime updatedAt;
+}
