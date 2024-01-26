4ca3c445adfa00c10ee192e8af6335c697b1e0aa
sso9594 <sso9594@naver.com>
2024-01-26 18:03:07 +0900

    feat: Review Read 모델 클래스 추가
    Related to: #183

--- be/review-query-api/src/main/java/com/zzaug/review/domain/model/review/query/ReviewQuery.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/model/review/query/ReviewQuery.java
@@ -1,10 +1,15 @@
 package com.zzaug.review.domain.model.review.query;
 
+import com.zzaug.review.entity.review.query.ReviewPoint;
+import com.zzaug.review.entity.review.query.ReviewType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Getter
 @ToString
@@ -13,5 +18,23 @@ import lombok.ToString;
 @Builder(toBuilder = true)
 public class ReviewQuery {
 
-	private String name;
+	private Long reviewId;
+
+	private Long questionId;
+
+	private String content;
+
+	private String author;
+
+	private Long authorId;
+
+	private LocalDateTime createdAt;
+
+	private LocalDateTime updatedAt;
+
+	private ReviewPoint startPoint;
+
+	private ReviewPoint endPoint;
+
+	private ReviewType tag;
 }
