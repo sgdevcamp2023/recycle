1bf81686c65239ef3fcc13aa2f11110a86254e16
sso9594 <sso9594@naver.com>
2024-01-26 18:03:42 +0900

    feat: Review Read 모델 repository 추가
    Related to: #183

--- be/review-query-api/src/main/java/com/zzaug/review/domain/persistence/review/ReviewQueryRepository.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/persistence/review/ReviewQueryRepository.java
@@ -1,6 +1,7 @@
 package com.zzaug.review.domain.persistence.review;
 
 import com.zzaug.review.entity.review.query.ReviewQueryEntity;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewQueryRepository extends JpaRepository<ReviewQueryEntity, Long> {}
+public interface ReviewQueryRepository extends ElasticsearchRepository<ReviewQueryEntity, Long> {}
