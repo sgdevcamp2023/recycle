4a7d908ef604cb19795cdf07e93c04a319b57a85
sso9594 <sso9594@naver.com>
2024-01-26 18:05:18 +0900

    feat: Review 도메인의 Read 모델 entity 클래스 및 enum, embedded 클래스 추가
    Related to: #183

---
+++ be/review-query-api/src/main/java/com/zzaug/review/entity/review/query/ReviewType.java
@@ -0,0 +1,15 @@
+package com.zzaug.review.entity.review.query;
+
+import lombok.Getter;
+
+@Getter
+public enum ReviewType {
+    CODE("code"),
+    LINE("line");
+
+    private String type;
+
+    ReviewType(String type) {
+        this.type = type;
+    }
+}
