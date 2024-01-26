88e4c48527882381d93f87bfb3c690c3b619702c
belljun3395 <195850@jnu.ac.kr>
2024-01-26 16:47:50 +0900

    feat: 구현 BlackTokenAuthRepository
    Related to: #156

---
+++ be/member-api/src/main/java/com/zzaug/member/persistence/auth/BlackTokenAuthRepository.java
@@ -0,0 +1,14 @@
+package com.zzaug.member.persistence.auth;
+
+import com.zzaug.member.entity.auth.BlackTokenAuthEntity;
+import com.zzaug.member.entity.auth.TokenData;
+import com.zzaug.member.persistence.support.checker.DeletedFalse;
+import com.zzaug.member.persistence.support.checker.ZzuagRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@ZzuagRepository
+public interface BlackTokenAuthRepository extends JpaRepository<BlackTokenAuthEntity, Long> {
+
+	@DeletedFalse
+	boolean existsByTokenAndDeletedFalse(TokenData token);
+}
