199d94270680f18c366ec54c67bab8b31c0575fe
belljun3395 <195850@jnu.ac.kr>
2024-01-28 03:29:47 +0900

    test: 구현 UpdateMemberUseCaseTest 중복 사항
    Related to: #165

---
+++ be/member-api/src/test/java/com/zzaug/member/domain/usecase/member/UpdateMemberUseCaseTest_EXIST_CERTIFICATION.java
@@ -0,0 +1,35 @@
+package com.zzaug.member.domain.usecase.member;
+
+import com.zzaug.member.MemberApp;
+import com.zzaug.member.domain.dto.member.UpdateMemberUseCaseRequest;
+import com.zzaug.member.domain.usecase.AbstractUseCaseTest;
+import com.zzaug.member.domain.usecase.config.mock.repository.UMockAuthenticationDao;
+import com.zzaug.member.domain.usecase.config.mock.repository.UMockMemberSourceDao;
+import com.zzaug.member.domain.usecase.config.mock.service.UMockMemberSourceQuery;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles(value = {"exist-certification"})
+@SpringBootTest(
+		classes = {MemberApp.class, UMockAuthenticationDao.class, UMockMemberSourceQuery.class})
+class UpdateMemberUseCaseTest_EXIST_CERTIFICATION extends AbstractUseCaseTest {
+
+	@Autowired private UpdateMemberUseCase updateMemberUseCase;
+
+	@Test
+	void 수정하려는_CERTIFICATION이_존재하면_수정할_수_없습니다() {
+		UpdateMemberUseCaseRequest request =
+				UpdateMemberUseCaseRequest.builder()
+						.memberId(UMockMemberSourceDao.MEMBER_ID)
+						.certification("edit@email.com")
+						.password(UMockAuthenticationDao.PASSWORD_SOURCE)
+						.build();
+
+		Assertions.assertThatThrownBy(() -> updateMemberUseCase.execute(request))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("이미 존재하는 아이디입니다.");
+	}
+}
