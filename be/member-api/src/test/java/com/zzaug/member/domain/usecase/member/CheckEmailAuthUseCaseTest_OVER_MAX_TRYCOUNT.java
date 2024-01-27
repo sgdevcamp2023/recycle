88a2cba64cd121b8a6682133ed57701c873264ba
belljun3395 <195850@jnu.ac.kr>
2024-01-28 02:20:18 +0900

    test: 구현 CheckEmailAuthUseCaseTest 예외 케이스
    최대 시도 횟수를 초과한 경우
    
    Related to: #164

---
+++ be/member-api/src/test/java/com/zzaug/member/domain/usecase/member/CheckEmailAuthUseCaseTest_OVER_MAX_TRYCOUNT.java
@@ -0,0 +1,50 @@
+package com.zzaug.member.domain.usecase.member;
+
+import com.zzaug.member.MemberApp;
+import com.zzaug.member.domain.dto.member.CheckEmailAuthUseCaseRequest;
+import com.zzaug.member.domain.usecase.AbstractUseCaseTest;
+import com.zzaug.member.domain.usecase.config.mock.repository.UMockEmailAuthDao;
+import com.zzaug.member.domain.usecase.config.mock.repository.UMockEmailAuthLogDao;
+import com.zzaug.member.domain.usecase.config.mock.repository.UMockExternalContactDao;
+import com.zzaug.member.domain.usecase.config.mock.service.UMockMemberSourceQuery;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles(value = {"over-max-try-count"})
+@SpringBootTest(
+		classes = {
+			MemberApp.class,
+			UMockExternalContactDao.class,
+			UMockEmailAuthDao.class,
+			UMockEmailAuthLogDao.class,
+			UMockMemberSourceQuery.class
+		})
+class CheckEmailAuthUseCaseTest_OVER_MAX_TRYCOUNT extends AbstractUseCaseTest {
+
+	@Autowired private CheckEmailAuthUseCase checkEmailAuthUseCase;
+
+	Long memberId = 1L;
+	String code = UMockEmailAuthDao.CODE;
+	String email = "sample@email.com";
+	String nonce = "nonce";
+	final CheckEmailAuthUseCaseRequest request =
+			CheckEmailAuthUseCaseRequest.builder()
+					.memberId(memberId)
+					.code(code)
+					.email(email)
+					.nonce(nonce)
+					.build();
+
+	@Test
+	void 이메일_인증시도_초과로_인한_실패() {
+		// Given
+
+		// When
+		Assertions.assertThatThrownBy(() -> checkEmailAuthUseCase.execute(request))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("request try count is over");
+	}
+}
