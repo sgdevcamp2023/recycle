160ceaae40f0b5634fab5d40065d5fc73d566901
belljun3395 <195850@jnu.ac.kr>
2024-01-27 17:12:24 +0900

    feat: 구현 EmailAuthDaoImpl
    Related to: #163

---
+++ be/member-api/src/main/java/com/zzaug/member/domain/external/dao/auth/EmailAuthDaoImpl.java
@@ -0,0 +1,29 @@
+package com.zzaug.member.domain.external.dao.auth;
+
+import com.zzaug.member.entity.auth.EmailAuthEntity;
+import com.zzaug.member.entity.auth.EmailData;
+import com.zzaug.member.persistence.auth.EmailAuthRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Repository;
+
+@Profile("!usecase-test")
+@Repository
+@RequiredArgsConstructor
+public class EmailAuthDaoImpl implements EmailAuthDao {
+
+	private final EmailAuthRepository emailAuthRepository;
+
+	@Override
+	public Optional<EmailAuthEntity> findByMemberIdAndEmailAndNonceAndDeletedFalse(
+			Long memberId, EmailData email, String nonce) {
+		return emailAuthRepository.findByMemberIdAndEmailAndNonceAndDeletedFalse(
+				memberId, email, nonce);
+	}
+
+	@Override
+	public EmailAuthEntity saveEmailAuth(EmailAuthEntity emailAuthEntity) {
+		return emailAuthRepository.save(emailAuthEntity);
+	}
+}
