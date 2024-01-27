cc0008f868ec011908b22d906302b3a3efbfae06
belljun3395 <195850@jnu.ac.kr>
2024-01-28 03:29:01 +0900

    test: 구현 UpdateMemberUseCaseTest
    Related to: #165

---
+++ be/member-api/src/test/java/com/zzaug/member/domain/usecase/member/UpdateMemberUseCaseTest.java
@@ -0,0 +1,45 @@
+package com.zzaug.member.domain.usecase.member;
+
+import com.zzaug.member.MemberApp;
+import com.zzaug.member.domain.dto.member.UpdateMemberUseCaseRequest;
+import com.zzaug.member.domain.usecase.AbstractUseCaseTest;
+import com.zzaug.member.domain.usecase.config.mock.repository.UMockAuthenticationDao;
+import com.zzaug.member.domain.usecase.config.mock.repository.UMockMemberSourceDao;
+import com.zzaug.member.domain.usecase.config.mock.service.UMockMemberSourceQuery;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+		classes = {MemberApp.class, UMockAuthenticationDao.class, UMockMemberSourceQuery.class})
+class UpdateMemberUseCaseTest extends AbstractUseCaseTest {
+
+	@Autowired private UpdateMemberUseCase updateMemberUseCase;
+
+	@Test
+	void CERTIFICATION을_수정합니다() {
+		UpdateMemberUseCaseRequest request =
+				UpdateMemberUseCaseRequest.builder()
+						.memberId(UMockMemberSourceDao.MEMBER_ID)
+						.certification("edit@email.com")
+						.password(UMockAuthenticationDao.PASSWORD_SOURCE)
+						.build();
+
+		updateMemberUseCase.execute(request);
+	}
+
+	@Test
+	void 비밀번호가_일치하지_않으면_수정할_수_없습니다() {
+		UpdateMemberUseCaseRequest request =
+				UpdateMemberUseCaseRequest.builder()
+						.memberId(UMockMemberSourceDao.MEMBER_ID)
+						.certification(UMockAuthenticationDao.CERTIFICATION)
+						.password("wrong-password")
+						.build();
+
+		Assertions.assertThatThrownBy(() -> updateMemberUseCase.execute(request))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessageContaining("비밀번호가 일치하지 않습니다.");
+	}
+}
