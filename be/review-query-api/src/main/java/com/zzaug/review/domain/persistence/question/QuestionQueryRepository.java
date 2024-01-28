ded07d8422477ba29a4eaa17ec5e6791af0e3079
sso9594 <sso9594@naver.com>
2024-01-28 19:31:02 +0900

    feat: Question Query의 Repository 추가
    Related to: #195

--- be/review-query-api/src/main/java/com/zzaug/review/domain/persistence/question/QuestionQueryRepository.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/persistence/question/QuestionQueryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public interface QuestionQueryRepository
 		extends ElasticsearchRepository<QuestionQueryEntity, Long> {
