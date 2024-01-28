1537bdd42e902be2acfc21faf20844d2d47592dd
sso9594 <sso9594@naver.com>
2024-01-28 23:05:47 +0900

    refactor: elasticsearch 설정 변경에 대한 변경사항 반영
    Related to: #195

--- be/review-query-api/src/main/java/com/zzaug/review/entity/question/query/QuestionQueryEntity.java
+++ be/review-query-api/src/main/java/com/zzaug/review/entity/question/query/QuestionQueryEntity.java
@@ -1,12 +1,6 @@
 package com.zzaug.review.entity.question.query;
 
 import java.time.LocalDateTime;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.*;
@@ -21,8 +15,7 @@ import org.springframework.data.elasticsearch.annotations.*;
 @Setting(settingPath = "elasticsearch/settings/question-setting.json")
 public class QuestionQueryEntity {
 
-	@Id
-	private Long questionId;
+	@Id private Long questionId;
 
 	@Field(type = FieldType.Text)
 	private String content;
@@ -34,9 +27,15 @@ public class QuestionQueryEntity {
 
 	private int reviewCnt;
 
-	@Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS||epoch_millis")
+	@Field(
+			type = FieldType.Date,
+			format = DateFormat.custom,
+			pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS||epoch_millis")
 	private LocalDateTime createdAt;
 
-	@Field(type = FieldType.Date, format = DateFormat.custom, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS||epoch_millis")
+	@Field(
+			type = FieldType.Date,
+			format = DateFormat.custom,
+			pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS||epoch_millis")
 	private LocalDateTime updatedAt;
 }
