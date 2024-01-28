48a42d0039b5b74f74b3cfb597d0d2d43aae654b
krokerdile <crazykroker@gmail.com>
2024-01-28 19:18:18 +0900

    feat: 서버 Port 관련 Axios api 수정 - Port 별(서버 별)로 api 요청 날릴 수 있도록 작업 Related to axios API 기반으로 react-query custom hook 작성하기 #189

--- fe/src/api/axios.ts
+++ fe/src/api/axios.ts
@@ -1,15 +1,44 @@
-import axios from "axios";
+import axios, { AxiosInstance } from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL + "/api/v1/";
-const accessToken = localStorage.getItem("accessToken");
+const baseURL = import.meta.env.VITE_API_URL;
 
-const clientApi = axios.create({
-  baseURL,
-});
+interface ServiceEndpoints {
+  [key: string]: number;
+}
 
-clientApi.interceptors.request.use((config) => {
-  config.headers.Authorization = "Bearer " + accessToken;
-  return config;
-});
+const CreateServiceApi = (service: string): AxiosInstance => {
+  const portMap: ServiceEndpoints = {
+    review: 8082,
+    member: 8081,
+    notifictation: 8084,
+    // 다른 서비스들의 포트 번호 추가
+  };
+
+  const servicePort = portMap[service];
+  const serviceEndpoint = `${baseURL}:${servicePort}/api/v1`;
+  const api = axios.create({
+    baseURL: serviceEndpoint,
+  });
+
+  // Interceptor를 사용하여 accessToken을 요청에 추가
+  // const { authToken } = useAuthStore();
+  const accessToken = localStorage.getItem('accessToken');
+
+  api.interceptors.request.use((config) => {
+    config.headers.Authorization = accessToken;
+    return config;
+  });
+  return api;
+};
+
+interface ClientAPI {
+  [key: string]: AxiosInstance;
+}
+
+const clientApi: ClientAPI = {
+  review: CreateServiceApi('review'),
+  member: CreateServiceApi('member'),
+  notification: CreateServiceApi('notification'),
+};
 
 export default clientApi;
