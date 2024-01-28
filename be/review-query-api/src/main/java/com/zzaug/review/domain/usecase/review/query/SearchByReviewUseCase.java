61f2f79986b0d9dc425e2dea8dd496327ea03d22
sso9594 <sso9594@naver.com>
2024-01-28 21:45:30 +0900

    feat: 리뷰를 기준으로 검색 한 후 결과를 리턴하는 usecase 추가
    Related to: #212

---
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/SearchByReviewUseCase.java
@@ -0,0 +1,49 @@
+package com.zzaug.review.domain.usecase.review.query;
+
+import com.zzaug.review.domain.dto.review.query.SearchByReviewUseCaseRequest;
+import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
+import com.zzaug.review.domain.persistence.review.ReviewQueryRepository;
+import com.zzaug.review.entity.question.query.QuestionQueryEntity;
+import com.zzaug.review.entity.review.query.ReviewQueryEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class SearchByReviewUseCase {
+
+    private final ReviewQueryRepository reviewQueryRepository;
+    private final QuestionQueryRepository questionQueryRepository;
+
+    @Transactional
+    public List<Map<String, Object>> execute (SearchByReviewUseCaseRequest request){
+        List<ReviewQueryEntity> reviewResult = reviewQueryRepository.findAllByAuthorIdAndContentContaining(request.getAuthorId(), request.getQuery());
+        LinkedHashSet<QuestionQueryEntity> questionResult = new LinkedHashSet<>();
+
+        for(ReviewQueryEntity reviewQueryEntity : reviewResult){
+            Long questionId = reviewQueryEntity.getQuestionId();
+            QuestionQueryEntity result = questionQueryRepository.findByQuestionId(questionId);
+            questionResult.add(result);
+        }
+
+        List<Map<String, Object>> responses = new ArrayList<>();
+
+        for(QuestionQueryEntity question : questionResult){
+            Map<String, Object> questionMap = new HashMap<>();
+            questionMap.put("question", question);
+            List<ReviewQueryEntity> relatedReviews = reviewResult.stream()
+                    .filter(ReviewQueryEntity -> ReviewQueryEntity.getQuestionId().equals(question.getQuestionId()))
+                    .collect(Collectors.toList());
+            questionMap.put("reviews", relatedReviews);
+            responses.add(questionMap);
+        }
+
+        return responses;
+    }
+}
