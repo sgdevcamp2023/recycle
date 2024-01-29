b5b8550b4432e0cc99e053b4f68fb05d7ed0b562
sso9594 <sso9594@naver.com>
2024-01-29 19:03:04 +0900

    feat: Review 도메인 임시 저장 조회를 위한 메소드 추가
    Related to: #112

--- be/review-api/src/main/java/com/zzaug/review/domain/persistence/review/ReviewTempRepository.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/persistence/review/ReviewTempRepository.java
@@ -3,5 +3,11 @@ package com.zzaug.review.domain.persistence.review;
 import com.zzaug.review.entity.review.ReviewTempEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ReviewTempRepository extends CrudRepository<ReviewTempEntity, String> {
+
+    List<ReviewTempEntity> findAllByAuthorIdAndQuestionId(Long authorId, Long questionId);
+
+    List<ReviewTempEntity> findByTempId(String tempId);
 }
