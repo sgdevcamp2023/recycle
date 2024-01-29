e31f026251ae33e97fdf7894ede982504b98ba30
belljun3395 <195850@jnu.ac.kr>
2024-01-30 00:44:51 +0900

    feat: 구현 LoginLogDaoImpl#findTopByMemberIdAndStatusAndDeletedFalse
    Related to: #168

--- be/member-api/src/main/java/com/zzaug/member/domain/external/dao/log/LoginLogDaoImpl.java
+++ be/member-api/src/main/java/com/zzaug/member/domain/external/dao/log/LoginLogDaoImpl.java
@@ -1,7 +1,9 @@
 package com.zzaug.member.domain.external.dao.log;
 
 import com.zzaug.member.entity.log.LoginLogEntity;
+import com.zzaug.member.entity.log.LoginStatus;
 import com.zzaug.member.persistence.log.LoginLogRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
@@ -16,5 +18,11 @@ public class LoginLogDaoImpl implements LoginLogDao {
 	@Override
 	public LoginLogEntity saveLoginLog(LoginLogEntity entity) {
 		return loginLogRepository.save(entity);
+	}
+
+	@Override
+	public Optional<LoginLogEntity> findTopByMemberIdAndStatusAndDeletedFalse(
+			Long memberId, LoginStatus status) {
+		return loginLogRepository.findTopByMemberIdAndStatusAndDeletedFalse(memberId, status);
 	}
 }
