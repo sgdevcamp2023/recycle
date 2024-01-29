436d5d6f6c927d50d3ab7026d98eb22a4dc802bd
belljun3395 <195850@jnu.ac.kr>
2024-01-29 21:59:35 +0900

    refactor: 수정 공통로직을 분리한 클래스를 통해
    MemberContacts를 private가 아닌 public 클래스로 변경
    
    MemberContactExtractor를 통해 연락 수단 추출 로직 분리
    
    Related to: #167

--- be/member-api/src/main/java/com/zzaug/member/domain/usecase/member/GetMemberUseCase.java
+++ be/member-api/src/main/java/com/zzaug/member/domain/usecase/member/GetMemberUseCase.java
@@ -4,15 +4,13 @@ import com.zzaug.member.domain.dto.member.GetMemberUseCaseRequest;
 import com.zzaug.member.domain.dto.member.GetMemberUseCaseResponse;
 import com.zzaug.member.domain.external.dao.member.ExternalContactDao;
 import com.zzaug.member.domain.external.service.member.MemberSourceQuery;
+import com.zzaug.member.domain.model.member.MemberContacts;
 import com.zzaug.member.domain.model.member.MemberSource;
-import com.zzaug.member.entity.member.ContactType;
+import com.zzaug.member.domain.support.entity.MemberContactExtractor;
 import com.zzaug.member.entity.member.ExternalContactEntity;
 import java.util.List;
-import lombok.Builder;
-import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -34,34 +32,12 @@ public class GetMemberUseCase {
 		// get member's contacts
 		List<ExternalContactEntity> contacts =
 				externalContactRepository.findAllByMemberIdAndDeletedFalse(source.getId());
-		MemberContacts memberContacts = extract(contacts);
+		MemberContacts memberContacts = MemberContactExtractor.execute(contacts);
 
 		return GetMemberUseCaseResponse.builder()
 				.id(source.getId())
 				.email(memberContacts.getEmail())
 				.github(memberContacts.getGithub())
 				.build();
-	}
-
-	private MemberContacts extract(List<ExternalContactEntity> contacts) {
-		String email = Strings.EMPTY;
-		String github = Strings.EMPTY;
-		for (ExternalContactEntity contact : contacts) {
-			if (contact.getContactType().equals(ContactType.EMAIL)) {
-				email = contact.getSource();
-				continue;
-			}
-			if (contact.getContactType().equals(ContactType.GITHUB)) {
-				github = contact.getSource();
-			}
-		}
-		return MemberContacts.builder().email(email).github(github).build();
-	}
-
-	@Data
-	@Builder
-	private static class MemberContacts {
-		private final String email;
-		private final String github;
 	}
 }
