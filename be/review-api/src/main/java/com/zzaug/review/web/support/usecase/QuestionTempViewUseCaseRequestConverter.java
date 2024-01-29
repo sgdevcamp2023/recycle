29e2b871089b100bcb16966dcf435c1f834490a8
sso9594 <sso9594@naver.com>
2024-01-29 16:35:41 +0900

    feat: QuestionTempViewUsecase를 사용하기 위한 Request를 생성하는 converter 추가
    Related to: #98

---
+++ be/review-api/src/main/java/com/zzaug/review/web/support/usecase/QuestionTempViewUseCaseRequestConverter.java
@@ -0,0 +1,19 @@
+package com.zzaug.review.web.support.usecase;
+
+import com.zzaug.review.domain.dto.question.QuestionTempViewUseCaseRequest;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class QuestionTempViewUseCaseRequestConverter {
+    public static QuestionTempViewUseCaseRequest from(String tempId, Long authorId) {
+        return QuestionTempViewUseCaseRequest.builder()
+                .tempId(tempId)
+                .authorId(authorId)
+                .build();
+    }
+    public static QuestionTempViewUseCaseRequest from(Long authorId) {
+        return QuestionTempViewUseCaseRequest.builder()
+                .authorId(authorId)
+                .build();
+    }
+}
