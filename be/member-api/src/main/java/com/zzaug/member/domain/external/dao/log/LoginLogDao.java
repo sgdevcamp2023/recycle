cc0f118952807f5f69f5f0477544a3381ad23641
belljun3395 <195850@jnu.ac.kr>
2024-01-30 00:44:41 +0900

    feat: 선언 LoginLogDao#findTopByMemberIdAndStatusAndDeletedFalse 추가
    Related to: #168

--- be/member-api/src/main/java/com/zzaug/member/domain/external/dao/log/LoginLogDao.java
+++ be/member-api/src/main/java/com/zzaug/member/domain/external/dao/log/LoginLogDao.java
@@ -1,8 +1,13 @@
 package com.zzaug.member.domain.external.dao.log;
 
 import com.zzaug.member.entity.log.LoginLogEntity;
+import com.zzaug.member.entity.log.LoginStatus;
+import java.util.Optional;
 
 public interface LoginLogDao {
 
 	LoginLogEntity saveLoginLog(LoginLogEntity entity);
+
+	Optional<LoginLogEntity> findTopByMemberIdAndStatusAndDeletedFalse(
+			Long memberId, LoginStatus status);
 }
