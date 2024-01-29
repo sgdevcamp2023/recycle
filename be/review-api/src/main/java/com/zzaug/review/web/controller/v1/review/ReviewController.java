57c7bb907b6e882d385cd904933580f854c19678
sso9594 <sso9594@naver.com>
2024-01-29 19:07:33 +0900

    feat: Review 도메인 임시저장 및 조회 기능 구현에 따른 controller 반영
    Related to: #112

--- be/review-api/src/main/java/com/zzaug/review/web/controller/v1/review/ReviewController.java
+++ be/review-api/src/main/java/com/zzaug/review/web/controller/v1/review/ReviewController.java
@@ -1,30 +1,27 @@
 package com.zzaug.review.web.controller.v1.review;
 
-import com.zzaug.review.domain.dto.review.ReviewCreateUseCaseRequest;
-import com.zzaug.review.domain.dto.review.ReviewDeleteUseCaseRequest;
-import com.zzaug.review.domain.dto.review.ReviewEditUseCaseRequest;
-import com.zzaug.review.domain.dto.review.ReviewTempCreateUseCaseRequest;
+import com.zzaug.review.domain.dto.review.*;
 import com.zzaug.review.domain.exception.DataNotFoundException;
 import com.zzaug.review.domain.exception.UnauthorizedAuthorException;
-import com.zzaug.review.domain.usecase.review.ReviewCreateUseCase;
-import com.zzaug.review.domain.usecase.review.ReviewDeleteUseCase;
-import com.zzaug.review.domain.usecase.review.ReviewEditUseCase;
-import com.zzaug.review.domain.usecase.review.ReviewTempCreateUseCase;
+import com.zzaug.review.domain.usecase.review.*;
+import com.zzaug.review.entity.review.ReviewPoint;
+import com.zzaug.review.entity.review.ReviewType;
 import com.zzaug.review.support.ApiResponse;
 import com.zzaug.review.support.ApiResponseGenerator;
 import com.zzaug.review.support.MessageCode;
 import com.zzaug.review.web.dto.review.ReviewDeleteRequest;
 import com.zzaug.review.web.dto.review.ReviewRequest;
 import com.zzaug.review.web.dto.review.ReviewTempRequest;
-import com.zzaug.review.web.support.usecase.ReviewCreateUseCaseRequestConverter;
-import com.zzaug.review.web.support.usecase.ReviewDeleteUseCaseRequestConverter;
-import com.zzaug.review.web.support.usecase.ReviewEditUseCaseRequestConverter;
-import com.zzaug.review.web.support.usecase.ReviewTempUseCaseRequestConverter;
+import com.zzaug.review.web.support.usecase.*;
 import com.zzaug.security.authentication.token.TokenUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -35,6 +32,7 @@ public class ReviewController {
 	private final ReviewTempCreateUseCase reviewTempCreateUseCase;
 	private final ReviewEditUseCase reviewEditUseCase;
 	private final ReviewDeleteUseCase reviewDeleteUseCase;
+	private final ReviewTempViewUseCase reviewTempViewUseCase;
 
 	@PostMapping("/questions/{questionId}/reviews")
 	public ApiResponse<ApiResponse.SuccessBody<Void>> createReview(
@@ -62,7 +60,6 @@ public class ReviewController {
 
 		return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.RESOURCE_CREATED);
 	}
-
 	@PutMapping("/questions/{questionId}/reviews/{reviewId}")
 	public ApiResponse<ApiResponse.SuccessBody<Void>> editReview(
 			@AuthenticationPrincipal TokenUserDetails userDetails,
@@ -105,5 +102,25 @@ public class ReviewController {
 		}catch (UnauthorizedAuthorException e){
 			return ApiResponseGenerator.success(HttpStatus.OK, MessageCode.RESOURCE_DELETED);
 		}
+	}
+	@GetMapping("/questions/{questionId}/reviews/temp")
+	public ApiResponse<ApiResponse.SuccessBody<List<ReviewTempResponse>>> viewTempReviewList(
+			@AuthenticationPrincipal TokenUserDetails userDetails,
+			@PathVariable Long questionId,
+			@RequestParam(required = false) String tempId) {
+
+		ReviewTempViewUseCaseRequest useCaseRequest = new ReviewTempViewUseCaseRequest();
+
+		if (tempId == null){
+			useCaseRequest =
+					ReviewTempViewUseCaseRequestConverter.from(tempId);
+		} else {
+			useCaseRequest =
+					ReviewTempViewUseCaseRequestConverter.from(Long.valueOf(userDetails.getId()), questionId);
+		}
+
+		List<ReviewTempResponse> responses = reviewTempViewUseCase.execute(useCaseRequest);
+
+		return ApiResponseGenerator.success(responses, HttpStatus.OK, MessageCode.SUCCESS);
 	}
 }
