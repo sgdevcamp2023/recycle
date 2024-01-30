569d87f924934c2db52bdc702b6be52e3938f3fa
sso9594 <sso9594@naver.com>
2024-01-30 13:39:08 +0900

    feat: 검색을 위한 메소드 추가
    Related to: #195

---
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/persistence/question/QuestionQueryRepository.java
@@ -0,0 +1,12 @@
+package com.zzaug.review.domain.persistence.question;
+
+import com.zzaug.review.entity.question.query.QuestionQueryEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface QuestionQueryRepository
+		extends ElasticsearchRepository<QuestionQueryEntity, Long> {
+	Page<QuestionQueryEntity> findAllByAuthorIdAndContentContaining(
+			Pageable pageable, Long authorId, String query);
+}
