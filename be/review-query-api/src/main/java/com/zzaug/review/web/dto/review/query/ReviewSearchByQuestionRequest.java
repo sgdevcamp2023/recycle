62ae480f44d19919d0983ca0278c98f356d3aef9
sso9594 <sso9594@naver.com>
2024-01-30 16:56:29 +0900

    feat: 검색을 위한 request 클래스 추가
    Related to: #212

---
+++ be/review-query-api/src/main/java/com/zzaug/review/web/dto/review/query/ReviewSearchByQuestionRequest.java
@@ -0,0 +1,13 @@
+package com.zzaug.review.web.dto.review.query;
+
+import lombok.*;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class ReviewSearchByQuestionRequest {
+    private Long authorId;
+    private String query;
+}
---
+++ be/review-query-api/src/main/java/com/zzaug/review/web/dto/review/query/ReviewSearchRequest.java
@@ -0,0 +1,14 @@
+package com.zzaug.review.web.dto.review.query;
+
+import lombok.*;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class ReviewSearchRequest {
+    private Long authorId;
+    private String query;
+
+}
