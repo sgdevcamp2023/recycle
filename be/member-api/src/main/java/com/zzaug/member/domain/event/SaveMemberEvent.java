4245276584de096f6abbddb41943c1da0e17b439
belljun3395 <195850@jnu.ac.kr>
2024-01-31 15:02:47 +0900

    feat: 구현 SaveMemberEvent
    Related to: #161

---
+++ be/member-api/src/main/java/com/zzaug/member/domain/event/SaveMemberEvent.java
@@ -0,0 +1,25 @@
+package com.zzaug.member.domain.event;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class SaveMemberEvent {
+
+	private Long memberId;
+	private String memberCertification;
+	@Builder.Default private LocalDateTime eventAt = LocalDateTime.now();
+}
