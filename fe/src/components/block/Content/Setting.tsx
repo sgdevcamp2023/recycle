da6e0831deac44afd6b671bf6ba658d0b6b37aa3
krokerdile <crazykroker@gmail.com>
2024-01-31 15:43:01 +0900

    feat: Setting getMemberInfo() 적용 Related Member #228

--- fe/src/components/block/Content/Setting.tsx
+++ fe/src/components/block/Content/Setting.tsx
@@ -1,15 +1,32 @@
 import { DefaultTabType } from '@store/useTabStore';
 import DefaultTab from '../navbar/DefaultTab';
 import styled from 'styled-components';
+import useGetMemberInfo from '@hooks/query/member/useGetMemberInfo';
+import { useState } from 'react';
+import Text from '@components/atom/Text';
 
 const Setting = () => {
   const items: Record<string, DefaultTabType> = {
-    '작성한 질문 목록': 'myQuestion',
-    '임시 저장된 질문': 'questionDrafts',
+    '내 정보': 'myMemberInfo',
   };
+
+  const [id, setId] = useState<number>(1);
+
+  const { data: memberInfo, isLoading } = useGetMemberInfo({ id });
+  console.log(memberInfo);
+  console.log(isLoading);
   return (
     <BoxWrapper>
       <DefaultTab items={items} />
+      {!isLoading ? (
+        <>
+          <Text>{memberInfo?.data?.data.id}</Text>
+          <Text>{memberInfo?.data?.data.email}</Text>
+          <Text>{memberInfo?.data?.data.github}</Text>
+        </>
+      ) : (
+        <>Loading 중입니다.</>
+      )}
     </BoxWrapper>
   );
 };
