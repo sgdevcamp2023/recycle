85231ab1dd77ae1d309ea30b90e08e0d2fd82ae2
sso9594 <sso9594@naver.com>
2024-01-31 14:47:56 +0900

    feat: QuestionQueryResponse 클래스 해당 브랜치에 추가
    Related to: #225

---
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/dto/question/query/QuestionQueryResponse.java
@@ -0,0 +1,21 @@
+package com.zzaug.review.domain.dto.question.query;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import lombok.*;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class QuestionQueryResponse {
+    private Long questionId;
+    private String content;
+    private String author;
+    private Long authorId;
+    private int reviewCnt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
