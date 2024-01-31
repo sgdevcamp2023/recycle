f43c78ee6889871186c7583889a983407225860a
Klomachenko <leekyumin0901@naver.com>
2024-01-31 18:23:55 +0900

    feat: token console check Description: 토큰을 콘솔로 확인합니다

--- fe/src/hooks/query/member/useLogin.ts
+++ fe/src/hooks/query/member/useLogin.ts
@@ -4,17 +4,29 @@ import { useMutation } from '@tanstack/react-query';
 const useLogin = () => {
   return useMutation({
     mutationFn: memberApi.login,
-    onSuccess: () => {
+    onSuccess: (data) => {
       console.log('요청 성공');
+      console.log(data);
+
+      const tokenData = data?.data?.data
+
+      const access = tokenData.accessToken;
+      const refresh = tokenData.refreshToken;
+      console.log(access,refresh)
+      if (access && refresh) {
+        console.log(access, refresh)
+      } else {
+        console.error('조졌다')
+      }
     },
     onError: (err) => {
       console.error('에러 발생');
       console.log(err);
     },
-    onSettled: (data) => {
-      console.log('결과에 관계 없이 무언가 실행됨');
-      console.log(data);
-    },
+    // onSettled: (data) => {
+    //   console.log('결과에 관계 없이 무언가 실행됨');
+    //   console.log(data);
+    // },
   });
 };
 
