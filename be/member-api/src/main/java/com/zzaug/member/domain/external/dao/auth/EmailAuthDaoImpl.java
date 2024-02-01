06eebccddb197852d7fad06680d6f9a0a01cd5e9
belljun3395 <195850@jnu.ac.kr>
2024-02-01 11:02:19 +0900

    feat: 구현 EmailAuthDao#saveEmailAuthSession
    Related to: #163

--- be/member-api/src/main/java/com/zzaug/member/domain/external/dao/auth/EmailAuthDaoImpl.java
+++ be/member-api/src/main/java/com/zzaug/member/domain/external/dao/auth/EmailAuthDaoImpl.java
@@ -3,6 +3,8 @@ package com.zzaug.member.domain.external.dao.auth;
 import com.zzaug.member.entity.auth.EmailAuthEntity;
 import com.zzaug.member.entity.auth.EmailData;
 import com.zzaug.member.persistence.auth.EmailAuthRepository;
+import com.zzaug.member.redis.email.EmailAuthSession;
+import com.zzaug.member.redis.email.EmailAuthSessionRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
@@ -15,6 +17,8 @@ public class EmailAuthDaoImpl implements EmailAuthDao {
 
 	private final EmailAuthRepository emailAuthRepository;
 
+	private final EmailAuthSessionRepository emailAuthSessionRepository;
+
 	@Override
 	public Optional<EmailAuthEntity> findByMemberIdAndEmailAndNonceAndDeletedFalse(
 			Long memberId, EmailData email, String nonce) {
@@ -25,5 +29,10 @@ public class EmailAuthDaoImpl implements EmailAuthDao {
 	@Override
 	public EmailAuthEntity saveEmailAuth(EmailAuthEntity emailAuthEntity) {
 		return emailAuthRepository.save(emailAuthEntity);
+	}
+
+	@Override
+	public EmailAuthSession saveEmailAuthSession(EmailAuthSession emailAuthSession) {
+		return emailAuthSessionRepository.save(emailAuthSession);
 	}
 }
