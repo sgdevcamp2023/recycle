af6b75d36c6c6df38d8ffebe311bd381af5c1829
belljun3395 <195850@jnu.ac.kr>
2024-02-01 19:20:07 +0900

    test: 반영 예외 변경
    Related to: #170

--- be/member-api/src/test/java/com/zzaug/member/domain/usecase/member/SearchMemberUseCaseTest_NOT_EXIST_CERTIFICATION.java
+++ be/member-api/src/test/java/com/zzaug/member/domain/usecase/member/SearchMemberUseCaseTest_NOT_EXIST_CERTIFICATION.java
@@ -2,10 +2,11 @@ package com.zzaug.member.domain.usecase.member;
 
 import com.zzaug.member.MemberApp;
 import com.zzaug.member.domain.dto.member.SearchMemberUseCaseRequest;
+import com.zzaug.member.domain.exception.SourceNotFoundException;
 import com.zzaug.member.domain.usecase.AbstractUseCaseTest;
 import com.zzaug.member.domain.usecase.config.mock.repository.UMockAuthenticationDao;
 import com.zzaug.member.domain.usecase.config.mock.repository.UMockExternalContactDao;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,14 +23,15 @@ class SearchMemberUseCaseTest_NOT_EXIST_CERTIFICATION extends AbstractUseCaseTes
 	void 없는_증명을_통해_회원_검색_요청을_진행합니다() {
 		// Given
 		SearchMemberUseCaseRequest request =
-				SearchMemberUseCaseRequest.builder().certification("not exist certification").build();
+				SearchMemberUseCaseRequest.builder().certification("notExist").build();
 
 		// When
-		IllegalArgumentException exception =
-				org.junit.jupiter.api.Assertions.assertThrows(
-						IllegalArgumentException.class, () -> searchMemberUseCase.execute(request));
+		SourceNotFoundException exception = Assertions.assertThrows(
+			SourceNotFoundException.class, () -> searchMemberUseCase.execute(request)
+		);
 
 		// Then
-		Assertions.assertThat(exception.getMessage()).isEqualTo("인증 정보가 존재하지 않습니다.");
+		org.assertj.core.api.Assertions.assertThat(exception.getMessage())
+			.contains("Certification");
 	}
 }
