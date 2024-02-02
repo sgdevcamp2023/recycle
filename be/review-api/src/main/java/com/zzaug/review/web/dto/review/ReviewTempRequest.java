d2806b012237bf97986cbbac46a73f2336791e19
sso9594 <sso9594@naver.com>
2024-02-02 14:30:38 +0900

    feat: request 유효성 검사를 위한 어노테이션 추가
    Related to: #112

--- be/review-api/src/main/java/com/zzaug/review/web/dto/review/ReviewTempRequest.java
+++ be/review-api/src/main/java/com/zzaug/review/web/dto/review/ReviewTempRequest.java
@@ -4,13 +4,17 @@ import com.zzaug.review.entity.review.ReviewPoint;
 import com.zzaug.review.entity.review.ReviewType;
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
 public class ReviewTempRequest {
+	@NotEmpty
 	private String tempId;
+	@NotEmpty
 	private String content;
 	private ReviewPoint startPoint;
 	private ReviewPoint endPoint;
