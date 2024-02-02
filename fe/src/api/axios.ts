667071b50b978c99e43640ec21c9644d9cb03d10
Klomachenko <leekyumin0901@naver.com>
2024-02-02 18:08:59 +0900

    feat: token reissuance 구현
    Related to 토큰 재발급 기능 구현 #248

--- fe/src/api/axios.ts
+++ fe/src/api/axios.ts
@@ -11,7 +11,7 @@ const CreateServiceApi = (service: string): AxiosInstance => {
   const portMap: ServiceEndpoints = {
     review: 8082,
     member: 8081,
-    notifictation: 8084,
+    notification: 8084,
     // 다른 서비스들의 포트 번호 추가
   };
 
@@ -28,9 +28,47 @@ const CreateServiceApi = (service: string): AxiosInstance => {
   // Interceptor를 사용하여 accessToken을 요청에 추가
   // const { authToken } = useAuthStore();
   // const accessToken = localStorage.getItem('accessToken');
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      const {
+        config,
+        response: { status },
+      } = error;
+
+      const originalRequest = config;
+
+      if (status === 403) {
+        const accessToken = localStorage.getItem('access');
+        const refreshToken = localStorage.getItem('refresh');
+
+        try {
+          const { data } = await axios({
+            method: 'post',
+            url: '/members/reissue',
+            data: { accessToken, refreshToken },
+          });
+          const newAccessToken = data.data.accessToken;
+          const newRefreshToken = data.data.refreshToken;
+          originalRequest.headers = {
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + newAccessToken,
+          };
+          localStorage.setItem('access', newAccessToken);
+          localStorage.setItem('refresh', newRefreshToken);
+          return await axios(originalRequest);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    },
+  );
 
   api.interceptors.request.use((config) => {
-    config.headers.Authorization = 'Bearer accessToken';
+    const accessToken = localStorage.getItem('access');
+    config.headers.Authorization = `Bearer ${accessToken}`;
     return config;
   });
   return api;
--- fe/src/hooks/query/member/useRefreshToken.ts
+++ fe/src/hooks/query/member/useRefreshToken.ts
@@ -1,22 +1,47 @@
-import memberApi from '@api/memberApi';
-import { useMutation } from '@tanstack/react-query';
+// import memberApi from '@api/memberApi';
+// import { useMutation } from '@tanstack/react-query';
+// import axios from 'axios';
 
-const useRefreshToken = () => {
-  return useMutation({
-    mutationFn: memberApi.refreshToken,
-    onSuccess: (data) => {
-      console.log('요청 성공');
-      console.log(data);
-    },
-    onError: (err) => {
-      console.error('에러 발생');
-      console.log(err);
-    },
-    onSettled: (data) => {
-      console.log('결과에 관계 없이 무언가 실행됨');
-      console.log(data);
-    },
-  });
-};
+// const useRefreshToken = () => {
+//   return useMutation({
+//     mutationFn: memberApi.refreshToken,
+//     onSuccess: async (data) => {
+//       console.log('요청 성공');
+//       console.log(data);
 
-export default useRefreshToken;
+//       const tokenData = data?.data?.data;
+
+//       const access = tokenData.accessToken;
+//       const refresh = tokenData.refreshToken;
+//       console.log(access, refresh);
+//       if (access && refresh) {
+//         localStorage.setItem('access', access);
+
+//         const lastApiEndpoint = localStorage.getItem('lastApiEndpoint');
+
+//         if (lastApiEndpoint !== null) {
+//           const response = await axios.get(lastApiEndpoint, {
+//             headers: {
+//               Authorization: `Bearer ${access}`,
+//             },
+//           });
+
+//           // 이후 response를 처리하거나 필요에 따라 상태 업데이트 등을 수행
+//           console.log('API 다시 요청 결과:', response.data);
+//         } else {
+//           console.error('마지막 API 엔드포인트가 없습니다.');
+//         }
+//       }
+//     },
+//     onError: (err) => {
+//       console.error('에러 발생');
+//       console.log(err);
+//     },
+//     onSettled: (data) => {
+//       console.log('결과에 관계 없이 무언가 실행됨');
+//       console.log(data);
+//     },
+//   });
+// };
+
+// export default useRefreshToken;
