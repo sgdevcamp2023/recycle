c40aa501061f6b66da881387ebea8705e22344f3
sso9594 <sso9594@naver.com>
2024-02-02 10:41:06 +0900

    refactor: 유효성 검사를 위한 NotEmpty, NotBlank 추가
    Related to: #98

--- be/review-api/src/main/java/com/zzaug/review/web/dto/question/QuestionTempRequest.java
+++ be/review-api/src/main/java/com/zzaug/review/web/dto/question/QuestionTempRequest.java
@@ -3,14 +3,17 @@ package com.zzaug.review.web.dto.question;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
 public class QuestionTempRequest {
-
+	@NotBlank
 	private String tempId;
-
+	@NotEmpty
 	private String content;
 }
