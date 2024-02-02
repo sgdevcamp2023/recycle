cdfd1e4f7595aa26333950fa636adb31b4d35d12
sso9594 <sso9594@naver.com>
2024-02-02 10:40:33 +0900

    refactor: 유효성 검사를 위한 NotEmpty 추가
    Related to: #98

--- be/review-api/src/main/java/com/zzaug/review/web/dto/question/QuestionRequest.java
+++ be/review-api/src/main/java/com/zzaug/review/web/dto/question/QuestionRequest.java
@@ -2,11 +2,14 @@ package com.zzaug.review.web.dto.question;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
 public class QuestionRequest {
+	@NotEmpty
 	private String content;
 }
