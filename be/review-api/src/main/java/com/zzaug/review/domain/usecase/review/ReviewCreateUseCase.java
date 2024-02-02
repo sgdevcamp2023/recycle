3225233d3ecf977f3d502113347fca7cacdb5531
sso9594 <sso9594@naver.com>
2024-02-02 14:29:29 +0900

    feat: 리뷰 생성 시 리뷰 카운트를 증가하는 메소드 추가
    Related to: #112

--- be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewCreateUseCase.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewCreateUseCase.java
@@ -3,6 +3,7 @@ package com.zzaug.review.domain.usecase.review;
 import com.zzaug.review.domain.dto.review.ReviewResponse;
 import com.zzaug.review.domain.dto.review.ReviewCreateUseCaseRequest;
 import com.zzaug.review.domain.model.review.Review;
+import com.zzaug.review.domain.persistence.question.QuestionRepository;
 import com.zzaug.review.domain.persistence.review.ReviewRepository;
 import com.zzaug.review.domain.support.entity.ReviewEntityConverter;
 import com.zzaug.review.domain.usecase.review.converter.ReviewConverter;
@@ -21,10 +22,17 @@ public class ReviewCreateUseCase {
 
 	private final ReviewRepository reviewRepository;
 	private final ReviewConverter reviewConverter;
+	private final QuestionRepository questionRepository;
 
 	@Transactional
 	public void execute(ReviewCreateUseCaseRequest request) {
 		Review review = reviewConverter.from(request);
 		reviewRepository.save(ReviewEntityConverter.from(review));
+		incReviewCount(request.getQuestionId());
+		// 리뷰 생성 이벤트 발행 ( effect : 리뷰 생성 및 리뷰 카운트 증가 )
+	}
+
+	private void incReviewCount(Long questionId) {
+		questionRepository.incReviewCnt(questionId);
 	}
 }
