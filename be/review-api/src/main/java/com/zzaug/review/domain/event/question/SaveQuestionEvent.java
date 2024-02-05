a107ac0f6d5230b3133a61ca47c4a9e127ee54d2
sso9594 <sso9594@naver.com>
2024-02-05 15:54:48 +0900

    feat: 질문 글 생성 이벤트 발급을 위한 event 모델 생성
    Related to: #258

---
+++ be/review-api/src/main/java/com/zzaug/review/domain/event/question/SaveQuestionEvent.java
@@ -0,0 +1,24 @@
+package com.zzaug.review.domain.event.question;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class SaveQuestionEvent {
+    private Long questionId;
+    private String content;
+    private String author;
+    private Long authorId;
+    private int reviewCnt;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime createdAt;
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime updatedAt;
+}
