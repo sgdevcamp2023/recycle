b14e9a8da9e22590f8a730b65a969c3ecd4cf782
belljun3395 <195850@jnu.ac.kr>
2024-02-05 15:03:08 +0900

    refactor: 수정 ListenerContainerFactory 설정 간소화
    구체적인 ListenerContainer의 설정은 ListenerContainer를 만들 때 설정할 수 있도록 최소한의 공통된 설정만을 남겨 두었습니다.
    
    Related to: #265

--- be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/ZRMQListenerConfig.java
+++ be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/ZRMQListenerConfig.java
@@ -1,43 +1,30 @@
 package com.zzaug.rabbitmq.config;
 
-import com.zzaug.rabbitmq.listener.ZRMQExceptionHandler;
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 @Import({ZRMQConnectionConfig.class})
 public class ZRMQListenerConfig {
 
-	private final int MAX_TRY_COUNT = 3;
-	private final int INITIAL_INTERVAL = 3000;
-	private final int MULTIPLIER = 3;
-	private final int MAX_INTERVAL = 10000;
-
 	private final ConnectionFactory connectionFactory;
 	private final MessageConverter messageConverter;
-	private final RabbitTemplate rabbitTemplate;
 
 	@Bean
 	SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory() {
 		final SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
 		factory.setConnectionFactory(connectionFactory);
-		factory.setDefaultRequeueRejected(false);
 		factory.setMessageConverter(messageConverter);
+		factory.setDefaultRequeueRejected(false);
 		factory.setChannelTransacted(true);
-		factory.setAdviceChain(
-				RetryInterceptorBuilder.stateless()
-						.maxAttempts(MAX_TRY_COUNT)
-						.recoverer(new ZRMQExceptionHandler(rabbitTemplate))
-						.backOffOptions(INITIAL_INTERVAL, MULTIPLIER, MAX_INTERVAL)
-						.build());
 		return factory;
 	}
 }
