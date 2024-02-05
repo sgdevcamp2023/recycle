437d3f33ac3ed37b7f0f2f771521e2a23afe9ef0
sso9594 <sso9594@naver.com>
2024-02-05 15:55:11 +0900

    feat: 저장한 entity를 event 모델로 변경하기 위한 converter 클래스 추가
    Related to: #258

---
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/question/converter/SaveQuestionEventConverter.java
@@ -0,0 +1,20 @@
+package com.zzaug.review.domain.usecase.question.converter;
+
+import com.zzaug.review.domain.event.question.SaveQuestionEvent;
+import com.zzaug.review.entity.question.QuestionEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SaveQuestionEventConverter {
+    public static SaveQuestionEvent from(QuestionEntity source) {
+        return SaveQuestionEvent.builder()
+                .questionId(source.getQuestionId())
+                .content(source.getContent())
+                .author(source.getAuthor())
+                .authorId(source.getAuthorId())
+                .createdAt(source.getCreatedAt())
+                .updatedAt(source.getUpdatedAt())
+                .reviewCnt(source.getReviewCnt())
+                .build();
+    }
+}
