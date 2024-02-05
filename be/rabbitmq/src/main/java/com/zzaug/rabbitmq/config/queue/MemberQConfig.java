63623c57e3109f82e9201f421e635c2f13f48f03
belljun3395 <195850@jnu.ac.kr>
2024-02-05 15:09:02 +0900

    refactor: 수정 Member, Review Queue
    Queue 생성시 인자를 추가하여 Dead Letter와 TTL과 관련된 설정을 추가하였습니다.
    
    Related to: #265

--- be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/queue/MemberQConfig.java
+++ be/rabbitmq/src/main/java/com/zzaug/rabbitmq/config/queue/MemberQConfig.java
@@ -12,11 +12,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MemberQConfig {
 
+	private static final String X_DEAD_LETTER_EXCHANGE_KEY = "x-dead-letter-exchange";
+	private static final String X_DEAD_LETTER_ROUTING_KEY = "x-dead-letter-routing-key";
+	private static final String X_MESSAGE_TTL_KEY = "x-message-ttl";
+	private static final Long X_MESSAGE_TTL = 1000 * 60 * 30L;
+
 	private static final String WILD_CARD = ".#";
 
 	@Bean
 	Queue memberQueue() {
-		return QueueBuilder.durable(ZRMQProperties.MEMBER_QUEUE_NAME).build();
+		return QueueBuilder.durable(ZRMQProperties.MEMBER_QUEUE_NAME)
+				.withArgument(X_DEAD_LETTER_EXCHANGE_KEY, ZRMQProperties.DEAD_LETTER_EXCHANGE_NAME)
+				.withArgument(X_DEAD_LETTER_ROUTING_KEY, ZRMQProperties.DEAD_LETTER_KEY_NAME + ".member")
+				.withArgument(X_MESSAGE_TTL_KEY, X_MESSAGE_TTL)
+				.build();
 	}
 
 	@Bean
