4516ea43fa63e74f4e1d306c04a3030f4f2c1d27
sso9594 <sso9594@naver.com>
2024-02-06 11:09:34 +0900

    refactor: 질문글 리뷰 카운트 로직 변경에 따른 entity 변경
    Related to: #269

--- be/review-api/src/main/java/com/zzaug/review/entity/question/QuestionEntity.java
+++ be/review-api/src/main/java/com/zzaug/review/entity/question/QuestionEntity.java
@@ -1,6 +1,5 @@
 package com.zzaug.review.entity.question;
 
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import javax.persistence.*;
 import lombok.*;
@@ -34,4 +33,12 @@ public class QuestionEntity {
 
 	@Column(name = "updated_at")
 	private LocalDateTime updatedAt;
+
+	public void incReviewCnt() {
+		this.reviewCnt++;
+	}
+
+	public void decReviewCnt() {
+		this.reviewCnt--;
+	}
 }
