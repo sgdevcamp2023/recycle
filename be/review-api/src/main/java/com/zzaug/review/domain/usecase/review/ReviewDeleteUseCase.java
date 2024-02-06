2092cf0ae0c1655b061b38ddcd34bfaad66de0cb
sso9594 <sso9594@naver.com>
2024-02-06 11:11:43 +0900

    feat: 리뷰 글 삭제 시 리뷰 삭제 이벤트 발행 로직 추가
    Related to: #269

--- be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewDeleteUseCase.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewDeleteUseCase.java
@@ -1,13 +1,19 @@
 package com.zzaug.review.domain.usecase.review;
 
 import com.zzaug.review.domain.dto.review.ReviewDeleteUseCaseRequest;
+import com.zzaug.review.domain.event.review.DeleteReviewEvent;
+import com.zzaug.review.domain.event.review.ReviewCntEvent;
 import com.zzaug.review.domain.persistence.question.QuestionRepository;
 import com.zzaug.review.domain.persistence.review.ReviewRepository;
+import com.zzaug.review.domain.usecase.review.event.converter.DeleteReviewEventConverter;
+import com.zzaug.review.domain.usecase.review.event.converter.ReviewCntEventConverter;
+import com.zzaug.review.entity.question.QuestionEntity;
 import com.zzaug.review.entity.review.ReviewEntity;
 import java.util.NoSuchElementException;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -16,6 +22,7 @@ import org.springframework.stereotype.Service;
 public class ReviewDeleteUseCase {
 	private final ReviewRepository reviewRepository;
 	private final QuestionRepository questionRepository;
+	private final ApplicationEventPublisher publisher;
 
 	@Transactional
 	public void execute(ReviewDeleteUseCaseRequest request) {
@@ -30,13 +37,30 @@ public class ReviewDeleteUseCase {
 
 		reviewRepository.deleteById(request.getReviewId());
 
-		decReviewCount(request.getQuestionId());
+		QuestionEntity resultDec = decReviewCount(request.getQuestionId());
 
 		// 리뷰 삭제 이벤트 발행 ( effect : 리뷰 삭제 및 리뷰 카운트 감소 )
 
+		publishEvent(DeleteReviewEventConverter.from(review));
+
+		publishEvent(ReviewCntEventConverter.from(resultDec));
+
+
 	}
 
-	private void decReviewCount(Long questionId) {
-		questionRepository.decReviewCnt(questionId);
+	private QuestionEntity decReviewCount(Long questionId) {
+		QuestionEntity target = questionRepository.findById(questionId)
+				.orElseThrow(() -> new NoSuchElementException("요청에 대한 응답을 찾을 수 없습니다."));
+		target.decReviewCnt();
+		return target;
 	}
+
+	private void publishEvent(DeleteReviewEvent event) {
+		publisher.publishEvent(event);
+	}
+
+	private void publishEvent(ReviewCntEvent event) {
+		publisher.publishEvent(event);
+	}
+
 }
