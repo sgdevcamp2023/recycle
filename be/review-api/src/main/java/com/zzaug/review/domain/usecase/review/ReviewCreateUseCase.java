4aea03708cc075acd2110291e8e6f54f1efd355a
sso9594 <sso9594@naver.com>
2024-02-06 11:11:19 +0900

    feat: 리뷰 글 생성 시 리뷰 생성 이벤트 발행 로직 추가
    Related to: #269

--- be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewCreateUseCase.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewCreateUseCase.java
@@ -1,15 +1,25 @@
 package com.zzaug.review.domain.usecase.review;
 
 import com.zzaug.review.domain.dto.review.ReviewCreateUseCaseRequest;
+import com.zzaug.review.domain.event.review.ReviewCntEvent;
+import com.zzaug.review.domain.event.review.SaveReviewEvent;
 import com.zzaug.review.domain.model.review.Review;
 import com.zzaug.review.domain.persistence.question.QuestionRepository;
 import com.zzaug.review.domain.persistence.review.ReviewRepository;
 import com.zzaug.review.domain.support.entity.ReviewEntityConverter;
 import com.zzaug.review.domain.usecase.review.converter.ReviewConverter;
 import javax.transaction.Transactional;
+
+import com.zzaug.review.domain.usecase.review.event.converter.ReviewCntEventConverter;
+import com.zzaug.review.domain.usecase.review.event.converter.SaveReviewEventConverter;
+import com.zzaug.review.entity.question.QuestionEntity;
+import com.zzaug.review.entity.review.ReviewEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
@@ -19,16 +29,31 @@ public class ReviewCreateUseCase {
 	private final ReviewRepository reviewRepository;
 	private final ReviewConverter reviewConverter;
 	private final QuestionRepository questionRepository;
+	private final ApplicationEventPublisher publisher;
 
 	@Transactional
 	public void execute(ReviewCreateUseCaseRequest request) {
 		Review review = reviewConverter.from(request);
-		reviewRepository.save(ReviewEntityConverter.from(review));
-		incReviewCount(request.getQuestionId());
+		ReviewEntity result = reviewRepository.save(ReviewEntityConverter.from(review));
+		QuestionEntity resultInc = incReviewCount(request.getQuestionId());
 		// 리뷰 생성 이벤트 발행 ( effect : 리뷰 생성 및 리뷰 카운트 증가 )
+		publishEvent(SaveReviewEventConverter.from(result));
+		publishEvent(ReviewCntEventConverter.from(resultInc));
+
 	}
 
-	private void incReviewCount(Long questionId) {
-		questionRepository.incReviewCnt(questionId);
+	private QuestionEntity incReviewCount(Long questionId) {
+		QuestionEntity target = questionRepository.findById(questionId)
+				.orElseThrow(() -> new NoSuchElementException("요청에 대한 응답을 찾을 수 없습니다."));
+		target.incReviewCnt();
+		return target;
+	}
+
+	private void publishEvent(SaveReviewEvent event){
+		publisher.publishEvent(event);
+	}
+
+	private void publishEvent(ReviewCntEvent event){
+		publisher.publishEvent(event);
 	}
 }
