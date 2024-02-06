1d0d9536abfa01da25c336b5bc312c4bdb5a2c69
belljun3395 <195850@jnu.ac.kr>
2024-02-06 16:24:07 +0900

    refactor: 수정 EnableRedisHttpSession JavaConfig로
    Related to: #271

--- be/member-api/src/main/java/com/zzaug/member/config/MemberSessionConfig.java
+++ be/member-api/src/main/java/com/zzaug/member/config/MemberSessionConfig.java
@@ -5,12 +5,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.http.CookieHttpSessionIdResolver;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @Configuration
+@EnableRedisHttpSession(redisNamespace = "zzuag:session")
 public class MemberSessionConfig {
 	public static final String SESSION_BEAN_NAME_PREFIX =
 			MemberAppConfig.BEAN_NAME_PREFIX + "Session";
