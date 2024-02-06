aee5a9353aebbf22d3b96eab2084d43f042cd08b
sso9594 <sso9594@naver.com>
2024-02-06 16:38:31 +0900

    refactor: request 클래스 유효성 검사를 위한 어노테이션 추가
    Related to: #134

--- be/review-api/src/main/java/com/zzaug/review/web/dto/review/CommentRequest.java
+++ be/review-api/src/main/java/com/zzaug/review/web/dto/review/CommentRequest.java
@@ -2,12 +2,15 @@ package com.zzaug.review.web.dto.review;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
 public class CommentRequest {
+	@NotBlank
 	private String content;
 	private Long parentId;
 }
