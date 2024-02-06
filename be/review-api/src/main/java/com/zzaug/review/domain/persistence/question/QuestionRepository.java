a163b0d888c64f1a8bf454e3a7f18ec587c43edb
sso9594 <sso9594@naver.com>
2024-02-06 11:09:53 +0900

    refactor: 질문글 리뷰 카운트 로직 변경에 따른 update 쿼리 삭제
    Related to: #269

--- be/review-api/src/main/java/com/zzaug/review/domain/persistence/question/QuestionRepository.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/persistence/question/QuestionRepository.java
@@ -5,14 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {
-	@Query(
-			"UPDATE QuestionEntity q SET q.reviewCnt = q.reviewCnt + 1 WHERE q.questionId = :questionId")
-	@Modifying
-	void incReviewCnt(Long questionId);
-
-	@Query(
-			"UPDATE QuestionEntity q SET q.reviewCnt = q.reviewCnt - 1 WHERE q.questionId = :questionId")
-	@Modifying
-	void decReviewCnt(Long questionId);
-}
+public interface QuestionRepository extends JpaRepository<QuestionEntity, Long> {}
