5e0be998a4d7fd89b2ea5fc2a3ae2a79cf20834a
sso9594 <sso9594@naver.com>
2024-02-06 14:54:46 +0900

    feat: 예외 처리 변경점 추가
    Related to: #112

--- be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewEditUseCase.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewEditUseCase.java
@@ -1,6 +1,8 @@
 package com.zzaug.review.domain.usecase.review;
 
 import com.zzaug.review.domain.dto.review.ReviewEditUseCaseRequest;
+import com.zzaug.review.domain.exception.AlreadyDeletedException;
+import com.zzaug.review.domain.exception.UnAuthorizationException;
 import com.zzaug.review.domain.persistence.review.ReviewRepository;
 import com.zzaug.review.entity.review.ReviewEntity;
 import java.util.NoSuchElementException;
@@ -23,8 +25,12 @@ public class ReviewEditUseCase {
 						.findById(request.getReviewId())
 						.orElseThrow(() -> new NoSuchElementException("요청에 대한 응답을 찾을 수 없습니다."));
 
+		if(review.isDeleted()) {
+			throw new AlreadyDeletedException("이미 삭제된 리뷰입니다.");
+		}
+
 		if (!review.getAuthorId().equals(request.getAuthorId())) {
-			throw new RuntimeException("접근 권한이 없습니다.");
+			throw new UnAuthorizationException("접근 권한이 없습니다.");
 		}
 
 		review.update(
