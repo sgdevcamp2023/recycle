4668bd2cb30fd3300fbf03e6658132a6715d81b0
sso9594 <sso9594@naver.com>
2024-02-06 11:12:54 +0900

    feat: 리뷰 글 삭제 이벤트 발행을 위한 이벤트 추가
    Related to: #269

---
+++ be/review-api/src/main/java/com/zzaug/review/domain/event/review/DeleteReviewEvent.java
@@ -0,0 +1,43 @@
+package com.zzaug.review.domain.event.review;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.zzaug.review.entity.review.ReviewPoint;
+import com.zzaug.review.entity.review.ReviewType;
+import lombok.*;
+
+import javax.persistence.Embedded;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class DeleteReviewEvent {
+
+    private Long reviewId;
+
+    private Long questionId;
+
+    private String content;
+
+    private String author;
+
+    private Long authorId;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime createdAt;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime updatedAt;
+
+    @Embedded private ReviewPoint startPoint;
+
+    @Embedded private ReviewPoint endPoint;
+
+    private ReviewType tag;
+
+    private boolean isDeleted;
+
+}
