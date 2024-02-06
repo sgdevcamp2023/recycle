7be2348ffd945fdffb8133a777bf03ec494032d2
belljun3395 <195850@jnu.ac.kr>
2024-02-06 15:39:22 +0900

    remove: 삭제 BlackToken 관련 클래스
    Related to: #271

--- be/member-api/src/main/java/com/zzaug/member/entity/auth/BlackTokenAuthEntity.java
+++ be/member-api/src/main/java/com/zzaug/member/entity/auth/BlackTokenAuthEntity.java
@@ -1,35 +1,35 @@
-package com.zzaug.member.entity.auth;
-
-import com.zzaug.member.entity.BaseEntity;
-import javax.persistence.Column;
-import javax.persistence.Convert;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Table;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
-import lombok.experimental.SuperBuilder;
-import org.hibernate.annotations.SQLDelete;
-
-@Entity
-@Getter
-@ToString(callSuper = true)
-@SuperBuilder(toBuilder = true)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
-@Table(name = "black_token_auth")
-@SQLDelete(sql = "UPDATE black_token_auth SET deleted=true where id=?")
-public class BlackTokenAuthEntity extends BaseEntity {
-
-	@Convert(converter = TokenDataConverter.class)
-	@Column(name = "token", nullable = false)
-	private TokenData token;
-
-	@Enumerated(EnumType.STRING)
-	@Column(name = "token_type", nullable = false)
-	private TokenType tokenType;
-}
+// package com.zzaug.member.entity.auth;
+//
+// import com.zzaug.member.entity.BaseEntity;
+// import javax.persistence.Column;
+// import javax.persistence.Convert;
+// import javax.persistence.Entity;
+// import javax.persistence.EnumType;
+// import javax.persistence.Enumerated;
+// import javax.persistence.Table;
+// import lombok.AccessLevel;
+// import lombok.AllArgsConstructor;
+// import lombok.Getter;
+// import lombok.NoArgsConstructor;
+// import lombok.ToString;
+// import lombok.experimental.SuperBuilder;
+// import org.hibernate.annotations.SQLDelete;
+//
+// @Entity
+// @Getter
+// @ToString(callSuper = true)
+// @SuperBuilder(toBuilder = true)
+// @NoArgsConstructor(access = AccessLevel.PROTECTED)
+// @AllArgsConstructor(access = AccessLevel.PRIVATE)
+// @Table(name = "black_token_auth")
+// @SQLDelete(sql = "UPDATE black_token_auth SET deleted=true where id=?")
+// public class BlackTokenAuthEntity extends BaseEntity {
+//
+//	@Convert(converter = TokenDataConverter.class)
+//	@Column(name = "token", nullable = false)
+//	private TokenData token;
+//
+//	@Enumerated(EnumType.STRING)
+//	@Column(name = "token_type", nullable = false)
+//	private TokenType tokenType;
+// }
--- be/member-api/src/main/java/com/zzaug/member/entity/auth/TokenDataConverter.java
+++ be/member-api/src/main/java/com/zzaug/member/entity/auth/TokenDataConverter.java
@@ -1,18 +1,18 @@
-package com.zzaug.member.entity.auth;
-
-import javax.persistence.AttributeConverter;
-import javax.persistence.Converter;
-
-@Converter(autoApply = true)
-public class TokenDataConverter implements AttributeConverter<TokenData, String> {
-
-	@Override
-	public String convertToDatabaseColumn(TokenData attribute) {
-		return attribute.getToken();
-	}
-
-	@Override
-	public TokenData convertToEntityAttribute(String dbData) {
-		return TokenData.builder().token(dbData).build();
-	}
-}
+// package com.zzaug.member.entity.auth;
+//
+// import javax.persistence.AttributeConverter;
+// import javax.persistence.Converter;
+//
+// @Converter(autoApply = true)
+// public class TokenDataConverter implements AttributeConverter<TokenData, String> {
+//
+//	@Override
+//	public String convertToDatabaseColumn(TokenData attribute) {
+//		return attribute.getToken();
+//	}
+//
+//	@Override
+//	public TokenData convertToEntityAttribute(String dbData) {
+//		return TokenData.builder().token(dbData).build();
+//	}
+// }
