0aa6bcf3b0a9839760bdd64bf04faad066f14098
belljun3395 <195850@jnu.ac.kr>
2024-02-07 13:39:03 +0900

    refactor: 수정 Mock을 통해 테스트가 통과할 수 있도록
    Related to: #275

--- be/member-api/src/test/java/com/zzaug/member/web/controller/v1/member/MemberControllerTest.java
+++ be/member-api/src/test/java/com/zzaug/member/web/controller/v1/member/MemberControllerTest.java
@@ -3,6 +3,8 @@ package com.zzaug.member.web.controller.v1.member;
 import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
 import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
 import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
@@ -14,6 +16,20 @@ import com.epages.restdocs.apispec.ResourceSnippetParameters;
 import com.epages.restdocs.apispec.Schema;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zzaug.member.MemberApp;
+import com.zzaug.member.domain.dto.member.GetMemberUseCaseResponse;
+import com.zzaug.member.domain.dto.member.MemberAuthToken;
+import com.zzaug.member.domain.dto.member.PostMemberUseCaseResponse;
+import com.zzaug.member.domain.dto.member.SearchMemberUseCaseResponse;
+import com.zzaug.member.domain.dto.member.UpdateMemberUseCaseResponse;
+import com.zzaug.member.domain.usecase.member.DeleteMemberUseCase;
+import com.zzaug.member.domain.usecase.member.GetMemberUseCase;
+import com.zzaug.member.domain.usecase.member.LogOutUseCase;
+import com.zzaug.member.domain.usecase.member.LoginUseCase;
+import com.zzaug.member.domain.usecase.member.PostMemberUseCase;
+import com.zzaug.member.domain.usecase.member.RenewalTokenUseCase;
+import com.zzaug.member.domain.usecase.member.SearchMemberUseCase;
+import com.zzaug.member.domain.usecase.member.UpdateMemberUseCase;
+import com.zzaug.member.web.controller.config.TestTokenUserDetailsService;
 import com.zzaug.member.web.controller.v1.description.Description;
 import com.zzaug.member.web.dto.member.LoginRequest;
 import com.zzaug.member.web.dto.member.MemberSaveRequest;
@@ -23,24 +39,40 @@ import javax.servlet.http.Cookie;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 @ActiveProfiles(value = "test")
 @AutoConfigureRestDocs
 @AutoConfigureMockMvc(addFilters = false)
-@SpringBootTest(classes = MemberApp.class)
+@SpringBootTest(classes = {MemberApp.class, TestTokenUserDetailsService.class})
 class MemberControllerTest {
 	@Autowired private MockMvc mockMvc;
 	@Autowired private ObjectMapper objectMapper;
+
+	@MockBean GetMemberUseCase getMemberUseCase;
+	@MockBean UpdateMemberUseCase updateMemberUseCase;
+	@MockBean PostMemberUseCase postMemberUseCase;
+	@MockBean DeleteMemberUseCase deleteMemberUseCase;
+	@MockBean LoginUseCase loginUseCase;
+	@MockBean LogOutUseCase logOutUseCase;
+	@MockBean RenewalTokenUseCase renewalTokenUseCase;
+	@MockBean SearchMemberUseCase searchMemberUseCase;
+
+	@Value("${token.test}")
+	private String testToken;
+
 	private static final String TAG = "MemberController";
 	private static final String BASE_URL = "/api/v1/members";
 
@@ -55,12 +87,20 @@ class MemberControllerTest {
 
 	@Test
 	@DisplayName(POST_BASE_URL_DNAME)
+	@WithUserDetails(userDetailsServiceBeanName = "testTokenUserDetailsService")
 	void save() throws Exception {
 		// set service mock
 		MemberSaveRequest request =
 				MemberSaveRequest.builder().certification(CERTIFICATION).password(PASSWORD).build();
-
 		String content = objectMapper.writeValueAsString(request);
+
+		when(postMemberUseCase.execute(any()))
+				.thenReturn(
+						PostMemberUseCaseResponse.builder()
+								.memberId(1L)
+								.certification(CERTIFICATION)
+								.password(PASSWORD)
+								.build());
 
 		mockMvc
 				.perform(
@@ -86,19 +126,29 @@ class MemberControllerTest {
 
 	@Test
 	@DisplayName(PUT_BASE_URL_DNAME)
+	@WithUserDetails(userDetailsServiceBeanName = "testTokenUserDetailsService")
 	void update() throws Exception {
 		// set service mock
 		MemberUpdateRequest request =
 				MemberUpdateRequest.builder().certification(CERTIFICATION).password(PASSWORD).build();
-
 		String content = objectMapper.writeValueAsString(request);
+		Cookie cookie = new Cookie("refreshToken", testToken);
+
+		when(updateMemberUseCase.execute(any()))
+				.thenReturn(
+						UpdateMemberUseCaseResponse.builder()
+								.accessToken(testToken)
+								.refreshToken(testToken)
+								.build());
+
 		mockMvc
 				.perform(
 						put(BASE_URL, 0)
 								.contentType(MediaType.APPLICATION_JSON)
 								.content(content)
 								.header(X_ZZAUG_ID, UUID.randomUUID())
-								.header(HttpHeaders.AUTHORIZATION, "Bearer accessToken"))
+								.header(HttpHeaders.AUTHORIZATION, "Bearer accessToken")
+								.cookie(cookie))
 				.andExpect(status().is2xxSuccessful())
 				.andDo(
 						document(
@@ -110,21 +160,33 @@ class MemberControllerTest {
 												.requestSchema(Schema.schema("UpdateMemberRequest"))
 												.requestHeaders(Description.authHeader(), Description.xZzaugIdHeader())
 												.responseSchema(Schema.schema("UpdateMemberResponse"))
-												.responseFields(Description.modified())
+												.responseFields(
+														Description.success(
+																new FieldDescriptor[] {
+																	fieldWithPath("data")
+																			.type(JsonFieldType.OBJECT)
+																			.description("data"),
+																	fieldWithPath("data.accessToken")
+																			.type(JsonFieldType.STRING)
+																			.description("어세스 토큰"),
+																}))
 												.build())));
 	}
 
 	@Test
 	@DisplayName(DELETE_BASE_URL_DNAME)
+	@WithUserDetails(userDetailsServiceBeanName = "testTokenUserDetailsService")
 	void delete() throws Exception {
 		// set service mock
+		Cookie cookie = new Cookie("refreshToken", testToken);
 
 		mockMvc
 				.perform(
 						RestDocumentationRequestBuilders.delete(BASE_URL, 0)
 								.contentType(MediaType.APPLICATION_JSON)
 								.header(X_ZZAUG_ID, UUID.randomUUID())
-								.header(HttpHeaders.AUTHORIZATION, "Bearer accessToken"))
+								.header(HttpHeaders.AUTHORIZATION, "Bearer accessToken")
+								.cookie(cookie))
 				.andExpect(status().is2xxSuccessful())
 				.andDo(
 						document(
@@ -142,12 +204,16 @@ class MemberControllerTest {
 
 	@Test
 	@DisplayName(POST_BASE_URL_DNAME + "/login")
+	@WithUserDetails(userDetailsServiceBeanName = "testTokenUserDetailsService")
 	void login() throws Exception {
 		// set service mock
 		LoginRequest request =
 				LoginRequest.builder().certification(CERTIFICATION).password(PASSWORD).build();
-
 		String content = objectMapper.writeValueAsString(request);
+
+		when(loginUseCase.execute(any()))
+				.thenReturn(
+						MemberAuthToken.builder().accessToken(testToken).refreshToken(testToken).build());
 
 		mockMvc
 				.perform(
@@ -155,7 +221,8 @@ class MemberControllerTest {
 								.contentType(MediaType.APPLICATION_JSON)
 								.content(content)
 								.header(X_ZZAUG_ID, UUID.randomUUID())
-								.header(HttpHeaders.AUTHORIZATION, "Bearer accessToken"))
+								.header(HttpHeaders.AUTHORIZATION, "Bearer accessToken")
+								.header(HttpHeaders.USER_AGENT, "agent"))
 				.andExpect(status().is2xxSuccessful())
 				.andDo(
 						document(
@@ -184,10 +251,10 @@ class MemberControllerTest {
 
 	@Test
 	@DisplayName(POST_BASE_URL_DNAME + "/logout")
+	@WithUserDetails(userDetailsServiceBeanName = "testTokenUserDetailsService")
 	void logout() throws Exception {
 		// set service mock
-
-		Cookie cookie = new Cookie("refreshToken", "refreshToken");
+		Cookie cookie = new Cookie("refreshToken", testToken);
 
 		mockMvc
 				.perform(
@@ -213,8 +280,16 @@ class MemberControllerTest {
 
 	@Test
 	@DisplayName(GET_BASE_URL_DNAME + "/{id}")
+	@WithUserDetails(userDetailsServiceBeanName = "testTokenUserDetailsService")
 	void read() throws Exception {
 		// set service mock
+		when(getMemberUseCase.execute(any()))
+				.thenReturn(
+						GetMemberUseCaseResponse.builder()
+								.id(1L)
+								.email("sample@email.com")
+								.github("github")
+								.build());
 
 		mockMvc
 				.perform(
@@ -255,14 +330,22 @@ class MemberControllerTest {
 
 	@Test
 	@DisplayName(GET_BASE_URL_DNAME)
+	@WithUserDetails(userDetailsServiceBeanName = "testTokenUserDetailsService")
 	void search() throws Exception {
 		// set service mock
+		when(searchMemberUseCase.execute(any()))
+				.thenReturn(
+						SearchMemberUseCaseResponse.builder()
+								.id(1L)
+								.email("sample@email.com")
+								.github("github")
+								.build());
 
 		mockMvc
 				.perform(
 						get(BASE_URL, 0)
 								.contentType(MediaType.APPLICATION_JSON)
-								.queryParam("certification", "{certification}")
+								.queryParam("certification", CERTIFICATION)
 								.header(X_ZZAUG_ID, UUID.randomUUID())
 								.header(HttpHeaders.AUTHORIZATION, "Bearer accessToken"))
 				.andExpect(status().is2xxSuccessful())
@@ -299,10 +382,14 @@ class MemberControllerTest {
 
 	@Test
 	@DisplayName(POST_BASE_URL_DNAME + "/token")
+	@WithUserDetails(userDetailsServiceBeanName = "testTokenUserDetailsService")
 	void token() throws Exception {
 		// set service mock
+		Cookie cookie = new Cookie("refreshToken", testToken);
 
-		Cookie refreshTokenCookie = new Cookie("refreshToken", "refreshToken");
+		when(renewalTokenUseCase.execute(any()))
+				.thenReturn(
+						MemberAuthToken.builder().accessToken(testToken).refreshToken(testToken).build());
 
 		mockMvc
 				.perform(
@@ -310,7 +397,7 @@ class MemberControllerTest {
 								.contentType(MediaType.APPLICATION_JSON)
 								.header(X_ZZAUG_ID, UUID.randomUUID())
 								.header(HttpHeaders.AUTHORIZATION, "Bearer accessToken")
-								.cookie(refreshTokenCookie))
+								.cookie(cookie))
 				.andExpect(status().is2xxSuccessful())
 				.andDo(
 						document(
