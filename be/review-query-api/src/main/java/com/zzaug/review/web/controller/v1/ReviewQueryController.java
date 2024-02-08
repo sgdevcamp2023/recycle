365fd6b54952b75c272c6784ba5db01efcdfe875
sso9594 <sso9594@naver.com>
2024-02-08 14:11:51 +0900

    refactor: web 모듈 사용을 위한 import 변경
    Related to: #225

--- be/review-query-api/src/main/java/com/zzaug/review/web/controller/v1/ReviewQueryController.java
+++ be/review-query-api/src/main/java/com/zzaug/review/web/controller/v1/ReviewQueryController.java
@@ -4,9 +4,7 @@ import com.zzaug.review.domain.dto.review.query.*;
 import com.zzaug.review.domain.usecase.review.query.ReviewByQuestionUseCase;
 import com.zzaug.review.domain.usecase.review.query.SearchByQuestionUseCase;
 import com.zzaug.review.domain.usecase.review.query.SearchByReviewUseCase;
-import com.zzaug.review.support.ApiResponse;
-import com.zzaug.review.support.ApiResponseGenerator;
-import com.zzaug.review.support.MessageCode;
+
 import com.zzaug.review.web.dto.review.query.ReviewSearchByQuestionRequest;
 import com.zzaug.review.web.dto.review.query.ReviewSearchRequest;
 import com.zzaug.review.web.support.usecase.ReviewByQuestionUseCaseRequestConverter;
@@ -16,6 +14,10 @@ import com.zzaug.security.authentication.token.TokenUserDetails;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+
+import com.zzaug.web.support.ApiResponse;
+import com.zzaug.web.support.ApiResponseGenerator;
+import com.zzaug.web.support.MessageCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
