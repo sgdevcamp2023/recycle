5b8e4a237d6830fd4a9761139f014e7c4ea811f7
belljun3395 <195850@jnu.ac.kr>
2024-02-12 13:25:20 +0900

    feat: 추가 TokenInvalidExceptionHandlerFilter에서 InvalidTokenAccessEvent 발행
    Related to: #289

--- be/security/src/main/java/com/zzaug/security/filter/exception/TokenInvalidExceptionHandlerFilter.java
+++ be/security/src/main/java/com/zzaug/security/filter/exception/TokenInvalidExceptionHandlerFilter.java
@@ -1,17 +1,23 @@
 package com.zzaug.security.filter.exception;
 
+import com.zzaug.security.event.InvalidTokenAccessEvent;
 import com.zzaug.security.exception.AccessTokenInvalidException;
+import com.zzaug.security.filter.token.AccessTokenResolver;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@RequiredArgsConstructor
 public class TokenInvalidExceptionHandlerFilter extends OncePerRequestFilter {
 
 	private static final String CONTENT_TYPE = "application/json; charset=UTF-8";
 	private static final ErrorResponse ERROR_RESPONSE = new ErrorResponse();
+	private final ApplicationEventPublisher applicationEventPublisher;
 
 	@Override
 	protected void doFilterInternal(
@@ -21,7 +27,21 @@ public class TokenInvalidExceptionHandlerFilter extends OncePerRequestFilter {
 			filterChain.doFilter(request, response);
 		} catch (AccessTokenInvalidException e) {
 			setError(response, e);
+			publishEvent(request);
 		}
+	}
+
+	private void publishEvent(HttpServletRequest request) {
+		String token = resolveAccessToken(request);
+		String ip = request.getRemoteAddr();
+		String userAgent = request.getHeader("User-Agent");
+		applicationEventPublisher.publishEvent(
+				InvalidTokenAccessEvent.builder().token(token).ip(ip).userAgent(userAgent).build());
+	}
+
+	private String resolveAccessToken(HttpServletRequest request) {
+		String authorization = request.getHeader("Authorization");
+		return AccessTokenResolver.resolve(authorization);
 	}
 
 	private void setError(HttpServletResponse response, Exception e) throws IOException {
