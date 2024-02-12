d1fe84d0b9d56af85a37ac9ec20fd921c1e09cea
belljun3395 <195850@jnu.ac.kr>
2024-02-12 13:01:47 +0900

    refactor: 적용 MemberSecurityChainedTransactional
    Related to: #291

--- be/member-api/src/main/java/com/zzaug/member/domain/usecase/member/LogOutUseCase.java
+++ be/member-api/src/main/java/com/zzaug/member/domain/usecase/member/LogOutUseCase.java
@@ -12,7 +12,7 @@ import com.zzaug.member.domain.model.log.LoginLog;
 import com.zzaug.member.domain.support.entity.LoginLogConverter;
 import com.zzaug.member.entity.log.LoginLogEntity;
 import com.zzaug.member.entity.log.LoginStatus;
-import com.zzaug.member.persistence.support.transaction.UseCaseTransactional;
+import com.zzaug.member.persistence.support.transaction.MemberSecurityChainedTransactional;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -35,7 +35,7 @@ public class LogOutUseCase {
 	private final EvictTokenCacheService evictWhiteTokenCacheServiceImpl;
 	private final EnrollTokenCacheService enrollBlackTokenCacheServiceImpl;
 
-	@UseCaseTransactional
+	@MemberSecurityChainedTransactional
 	public void execute(LogOutUseCaseRequest request) {
 		final Long memberId = request.getMemberId();
 		final String accessToken = request.getAccessToken();
