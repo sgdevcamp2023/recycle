48677289aecb7492e7fc24de8394007edae505ce
belljun3395 <195850@jnu.ac.kr>
2024-02-12 13:31:10 +0900

    !refactor: 추가 ErrorResponse의 반환에 timestamp 멤버

--- be/security/src/main/java/com/zzaug/security/filter/exception/TokenInvalidExceptionHandlerFilter.java
+++ be/security/src/main/java/com/zzaug/security/filter/exception/TokenInvalidExceptionHandlerFilter.java
@@ -4,6 +4,7 @@ import com.zzaug.security.event.InvalidTokenAccessEvent;
 import com.zzaug.security.exception.AccessTokenInvalidException;
 import com.zzaug.security.filter.token.AccessTokenResolver;
 import java.io.IOException;
+import java.time.LocalDateTime;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -16,7 +17,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class TokenInvalidExceptionHandlerFilter extends OncePerRequestFilter {
 
 	private static final String CONTENT_TYPE = "application/json; charset=UTF-8";
-	private static final ErrorResponse ERROR_RESPONSE = new ErrorResponse();
 	private final ApplicationEventPublisher applicationEventPublisher;
 
 	@Override
@@ -47,16 +47,28 @@ public class TokenInvalidExceptionHandlerFilter extends OncePerRequestFilter {
 	private void setError(HttpServletResponse response, Exception e) throws IOException {
 		response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 		response.setContentType(CONTENT_TYPE);
-		response.getWriter().write(ERROR_RESPONSE.toString());
+		ErrorResponse errorResponse = new ErrorResponse(LocalDateTime.now());
+		response.getWriter().write(errorResponse.toString());
 	}
 
+	@RequiredArgsConstructor
 	private static class ErrorResponse {
-		private String code = "fail.authentication";
-		private String message = "인증이 필요해요.";
+		private static String code = "fail.authentication";
+		private static String message = "인증이 필요해요.";
+		private final LocalDateTime timestamp;
 
 		@Override
 		public String toString() {
-			return "{" + "\"code\" : \"" + code + "\"" + ", \"message\" : \"" + message + "\"}";
+			return "{"
+					+ "\"code\" : \""
+					+ code
+					+ "\""
+					+ ", \"message\" : \""
+					+ message
+					+ "\""
+					+ ", \"timestamp\" : \""
+					+ timestamp
+					+ "\"}";
 		}
 	}
 }
