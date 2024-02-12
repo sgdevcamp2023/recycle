a80a6d2e992df0ac739c6d5a8726e6e6e5dc6db2
belljun3395 <195850@jnu.ac.kr>
2024-02-12 13:00:21 +0900

    feat: 구현 ChainedTransactionManager
    member의 transactionManager와 security의 transactionManager을 합쳤다.
    
    Related to: #291

--- be/member-api/src/main/java/com/zzaug/member/config/MemberJpaDataSourceConfig.java
+++ be/member-api/src/main/java/com/zzaug/member/config/MemberJpaDataSourceConfig.java
@@ -1,6 +1,7 @@
 package com.zzaug.member.config;
 
 import com.zzaug.flyway.datasource.EntityDataSource;
+import com.zzaug.security.config.SecurityJpaDataSourceConfig;
 import java.util.Map;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.transaction.ChainedTransactionManager;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -40,6 +42,8 @@ public class MemberJpaDataSourceConfig {
 			ENTITY_BEAN_NAME_PREFIX + "ManagerFactory";
 	public static final String TRANSACTION_MANAGER_NAME =
 			ENTITY_BEAN_NAME_PREFIX + "TransactionManager";
+	public static final String MEMBER_SECURITY_TRANSACTION_MANAGER_NAME =
+			"MemberSecurityTransactionManager";
 	public static final String DATASOURCE_NAME = ENTITY_BEAN_NAME_PREFIX + "DataSource";
 	private static final String JPA_PROPERTIES_NAME = ENTITY_BEAN_NAME_PREFIX + "JpaProperties";
 	private static final String HIBERNATE_PROPERTIES_NAME =
@@ -108,5 +112,13 @@ public class MemberJpaDataSourceConfig {
 	public PlatformTransactionManager transactionManager(
 			@Qualifier(ENTITY_MANAGER_FACTORY_NAME) EntityManagerFactory emf) {
 		return new JpaTransactionManager(emf);
+	}
+
+	@Bean(name = MEMBER_SECURITY_TRANSACTION_MANAGER_NAME)
+	public PlatformTransactionManager chainedTransactionManager(
+			@Qualifier(TRANSACTION_MANAGER_NAME) PlatformTransactionManager emf,
+			@Qualifier(SecurityJpaDataSourceConfig.TRANSACTION_MANAGER_NAME)
+					PlatformTransactionManager semf) {
+		return new ChainedTransactionManager(semf, emf);
 	}
 }
