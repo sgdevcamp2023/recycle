fd6225f993d3caf588ec12a1e276ceb19488fe8c
sso9594 <sso9594@naver.com>
2024-02-13 17:19:54 +0900

    feat: 리뷰 수정 이벤트를 전달받고 리뷰를 수정하는 로직 추가
    Related to: #293

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewQueryEditUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewQueryEditUseCase.java
@@ -1,13 +1,18 @@
 package com.zzaug.review.domain.usecase.review.query;
 
 import com.zzaug.review.domain.dto.review.query.ReviewQueryEditUseCaseRequest;
+import com.zzaug.review.domain.event.EditReviewEvent;
 import com.zzaug.review.domain.model.review.query.ReviewQuery;
 import com.zzaug.review.domain.persistence.review.ReviewQueryRepository;
 import com.zzaug.review.domain.support.entity.ReviewQueryEntityConverter;
 import com.zzaug.review.entity.review.query.ReviewQueryEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
@@ -16,10 +21,12 @@ public class ReviewQueryEditUseCase {
 	private final ReviewQueryRepository reviewQueryRepository;
 	private final ReviewQueryConverter reviewQueryConverter;
 
-	public void execute(ReviewQueryEditUseCaseRequest request) {
+	@Transactional
+	@EventListener
+	public void execute(EditReviewEvent event) {
 		ReviewQueryEntity reviewQueryEntity =
-				reviewQueryRepository.findById(request.getReviewId()).orElseThrow(RuntimeException::new);
-		ReviewQuery review = reviewQueryConverter.from(request, reviewQueryEntity);
+				reviewQueryRepository.findById(event.getReviewId()).orElseThrow(()-> new NoSuchElementException("Review not found"));
+		ReviewQuery review = reviewQueryConverter.from(event);
 		reviewQueryRepository.save(ReviewQueryEntityConverter.from(review));
 	}
 }
