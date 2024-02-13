c9acbc873b4655e35fc37fa5647d9ae993bc148e
sso9594 <sso9594@naver.com>
2024-02-13 14:07:13 +0900

    feat: application 이벤트를 받아 삭제를 처리하는 usecase …
    Related to: #292

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQueryDeleteUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQueryDeleteUseCase.java
@@ -1,24 +1,33 @@
 package com.zzaug.review.domain.usecase.question.query;
 
 import com.zzaug.review.domain.dto.question.query.QuestionQueryDeleteUseCaseRequest;
+import com.zzaug.review.domain.event.DeleteQuestionEvent;
+import com.zzaug.review.domain.model.question.query.QuestionQuery;
 import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
 import javax.transaction.Transactional;
+
+import com.zzaug.review.domain.support.entity.QuestionQueryEntityConverter;
+import com.zzaug.review.entity.question.query.QuestionQueryEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class QuestionQueryDeleteUseCase {
 	private final QuestionQueryRepository questionQueryRepository;
+	private final QuestionQueryConverter questionQueryConverter;
 
 	@Transactional
-	public void execute(QuestionQueryDeleteUseCaseRequest request) {
-		if (questionQueryRepository.findById(request.getQuestionId()).isPresent()) {
-			questionQueryRepository.deleteById(request.getQuestionId());
-		} else {
-			throw new RuntimeException("Question not found");
-		}
+	@EventListener
+	public void execute(DeleteQuestionEvent event) {
+		log.debug("Received event: {}", event);
+		QuestionQuery questionQuery = questionQueryConverter.from(event);
+		questionQueryRepository.save(QuestionQueryEntityConverter.from(questionQuery));
+		log.debug("QuestionQuery deleted: {}", questionQuery);
 	}
 }
