f3dcf57213d132f77d2a51ac69ded77aeb3e0dd6
sso9594 <sso9594@naver.com>
2024-02-13 18:06:13 +0900

    refactor: 리뷰 카운트 이벤트 제거에 따른 로직 삭제
    Related to: #269

--- be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewCreateUseCase.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewCreateUseCase.java
@@ -1,14 +1,12 @@
 package com.zzaug.review.domain.usecase.review;
 
 import com.zzaug.review.domain.dto.review.ReviewCreateUseCaseRequest;
-import com.zzaug.review.domain.event.review.ReviewCntEvent;
 import com.zzaug.review.domain.event.review.SaveReviewEvent;
 import com.zzaug.review.domain.model.review.Review;
 import com.zzaug.review.domain.persistence.question.QuestionRepository;
 import com.zzaug.review.domain.persistence.review.ReviewRepository;
 import com.zzaug.review.domain.support.entity.ReviewEntityConverter;
 import com.zzaug.review.domain.usecase.review.converter.ReviewConverter;
-import com.zzaug.review.domain.usecase.review.event.converter.ReviewCntEventConverter;
 import com.zzaug.review.domain.usecase.review.event.converter.SaveReviewEventConverter;
 import com.zzaug.review.entity.question.QuestionEntity;
 import com.zzaug.review.entity.review.ReviewEntity;
@@ -36,7 +34,6 @@ public class ReviewCreateUseCase {
 		QuestionEntity resultInc = incReviewCount(request.getQuestionId());
 		// 리뷰 생성 이벤트 발행 ( effect : 리뷰 생성 및 리뷰 카운트 증가 )
 		publishEvent(SaveReviewEventConverter.from(result));
-		publishEvent(ReviewCntEventConverter.from(resultInc));
 	}
 
 	private QuestionEntity incReviewCount(Long questionId) {
@@ -52,7 +49,4 @@ public class ReviewCreateUseCase {
 		publisher.publishEvent(event);
 	}
 
-	private void publishEvent(ReviewCntEvent event) {
-		publisher.publishEvent(event);
-	}
 }
