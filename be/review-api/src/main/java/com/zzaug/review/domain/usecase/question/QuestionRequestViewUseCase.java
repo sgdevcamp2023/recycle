ab507e18dfd2fb54ddbdcd7df8574a24648e6338
sso9594 <sso9594@naver.com>
2024-02-13 17:42:31 +0900

    style: 클래스 명 변경
    Related to: #258

--- be/review-api/src/main/java/com/zzaug/review/domain/usecase/question/QuestionRequestViewUseCase.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/question/QuestionRequestViewUseCase.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class QuestionReqViewUseCase {
+public class QuestionRequestViewUseCase {
 	private final QuestionRequestRepository questionRequestRepository;
 	private final QuestionRepository questionRepository;
 
