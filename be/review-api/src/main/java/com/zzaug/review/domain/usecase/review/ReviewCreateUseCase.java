72193acee985a61ecffbc0e3a8a20028e95f4a0c
sso9594 <sso9594@naver.com>
2024-02-13 20:08:06 +0900

    feat: 리뷰 생성 시 리뷰어 목록에 추가하는 로직 구현
    Related to: #269

--- be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewCreateUseCase.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/ReviewCreateUseCase.java
@@ -2,10 +2,13 @@ package com.zzaug.review.domain.usecase.review;
 
 import com.zzaug.review.domain.dto.review.ReviewCreateUseCaseRequest;
 import com.zzaug.review.domain.event.review.SaveReviewEvent;
+import com.zzaug.review.domain.model.member.ReviewerList;
 import com.zzaug.review.domain.model.review.Review;
+import com.zzaug.review.domain.persistence.member.ReviewerListRepository;
 import com.zzaug.review.domain.persistence.question.QuestionRepository;
 import com.zzaug.review.domain.persistence.review.ReviewRepository;
 import com.zzaug.review.domain.support.entity.ReviewEntityConverter;
+import com.zzaug.review.domain.support.entity.ReviewerListEntityConverter;
 import com.zzaug.review.domain.usecase.review.converter.ReviewConverter;
 import com.zzaug.review.domain.usecase.review.event.converter.SaveReviewEventConverter;
 import com.zzaug.review.entity.question.QuestionEntity;
@@ -25,6 +28,7 @@ public class ReviewCreateUseCase {
 	private final ReviewRepository reviewRepository;
 	private final ReviewConverter reviewConverter;
 	private final QuestionRepository questionRepository;
+	private final ReviewerListRepository reviewerListRepository;
 	private final ApplicationEventPublisher publisher;
 
 	@Transactional
@@ -32,6 +36,18 @@ public class ReviewCreateUseCase {
 		Review review = reviewConverter.from(request);
 		ReviewEntity result = reviewRepository.save(ReviewEntityConverter.from(review));
 		QuestionEntity resultInc = incReviewCount(request.getQuestionId());
+
+		//리뷰어 목록에 해당 사용자가 있는지 확인
+		if (!reviewerListRepository.existsByReviewerIdAndQuestionId(request.getAuthorId(), request.getQuestionId())) {
+			// 리뷰어 목록에 사용자가 없으면 리뷰어 목록에 추가
+			ReviewerList writeMember = ReviewerList.builder()
+					.reviewerId(request.getAuthorId())
+					.reviewerName(request.getAuthor())
+					.questionId(request.getQuestionId())
+					.build();
+			reviewerListRepository.save(ReviewerListEntityConverter.from(writeMember));
+		}
+
 		// 리뷰 생성 이벤트 발행 ( effect : 리뷰 생성 및 리뷰 카운트 증가 )
 		publishEvent(SaveReviewEventConverter.from(result));
 	}
