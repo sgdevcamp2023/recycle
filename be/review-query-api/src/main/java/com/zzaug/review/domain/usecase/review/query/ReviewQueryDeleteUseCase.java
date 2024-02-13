9cd60d28ec1236185247db01fc6a163f1eb7abdb
sso9594 <sso9594@naver.com>
2024-02-13 17:19:40 +0900

    feat: 리뷰 삭제 이벤트를 전달받고 리뷰를 생성 및 질문글의 리뷰 수 카운트를 감소하는 로직 추가
    Related to: #293

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewQueryDeleteUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewQueryDeleteUseCase.java
@@ -1,24 +1,38 @@
 package com.zzaug.review.domain.usecase.review.query;
 
 import com.zzaug.review.domain.dto.review.query.ReviewQueryDeleteUseCaseRequest;
+import com.zzaug.review.domain.event.DeleteReviewEvent;
+import com.zzaug.review.domain.model.review.query.ReviewQuery;
+import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
 import com.zzaug.review.domain.persistence.review.ReviewQueryRepository;
 import javax.transaction.Transactional;
+
+import com.zzaug.review.domain.support.entity.ReviewQueryEntityConverter;
+import com.zzaug.review.entity.question.query.QuestionQueryEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ReviewQueryDeleteUseCase {
 	private final ReviewQueryRepository reviewQueryRepository;
+	private final QuestionQueryRepository questionQueryRepository;
+	private final ReviewQueryConverter reviewQueryConverter;
 
 	@Transactional
-	public void execute(ReviewQueryDeleteUseCaseRequest request) {
-		if (reviewQueryRepository.findById(request.getReviewId()).isPresent()) {
-			reviewQueryRepository.deleteById(request.getReviewId());
-		} else {
-			throw new RuntimeException("Review not found");
-		}
+	@EventListener
+	public void execute(DeleteReviewEvent event) {
+		QuestionQueryEntity parentQuestion = questionQueryRepository.findById(event.getQuestionId())
+				.orElseThrow(() -> new NoSuchElementException("Question not found"));
+		ReviewQuery review = reviewQueryConverter.from(event);
+		reviewQueryRepository.save(ReviewQueryEntityConverter.from(review));
+
+		parentQuestion.decReviewCnt();
+		questionQueryRepository.save(parentQuestion);
 	}
 }
