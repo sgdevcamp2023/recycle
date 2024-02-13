2514e3f6dc8650bb7721f7a94921313594918e8b
sso9594 <sso9594@naver.com>
2024-02-13 18:14:01 +0900

    refactor: 질문글 이벤트를 발행하는 서비스 통합
    Related to: #258

--- be/review-api/src/main/java/com/zzaug/review/domain/usecase/question/event/QuestionEventService.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/question/event/QuestionEventService.java
@@ -1,5 +1,6 @@
 package com.zzaug.review.domain.usecase.question.event;
 
+import com.zzaug.review.domain.event.question.DeleteQuestionEvent;
 import com.zzaug.review.domain.event.question.SaveQuestionEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class CreateEventService {
+public class QuestionEventService {
 	@Value("${spring.rabbitmq.exchange}")
 	private String exchangeName;
 
@@ -22,6 +23,13 @@ public class CreateEventService {
 
 	@EventListener
 	public void sendEvent(SaveQuestionEvent event) {
+		log.debug("Event received: {}", event);
+		rabbitTemplate.convertAndSend(exchangeName, routingKey, event);
+		log.debug("Event sent: {}", event);
+	}
+
+	@EventListener
+	public void sendEvent(DeleteQuestionEvent event) {
 		log.debug("Event received: {}", event);
 		rabbitTemplate.convertAndSend(exchangeName, routingKey, event);
 		log.debug("Event sent: {}", event);
