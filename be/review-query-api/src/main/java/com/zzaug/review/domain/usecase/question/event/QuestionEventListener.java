1825fe0f5115112dcf5906134608db73c3d1f0ef
sso9594 <sso9594@naver.com>
2024-02-13 17:23:30 +0900

    refactor: 같은 큐에서 다른 객체를 전달 받을 때 처리하기 위한 handler 추가
    Related to: #292

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/event/QuestionEventListener.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/event/QuestionEventListener.java
@@ -4,6 +4,7 @@ import com.zzaug.review.domain.event.DeleteQuestionEvent;
 import com.zzaug.review.domain.event.SaveQuestionEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -11,16 +12,17 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@RabbitListener(queues = "zzaug.review")
 public class QuestionEventListener {
     private final ApplicationEventPublisher publisher;
 
-    @RabbitListener(queues = "zzaug.review")
+    @RabbitHandler
     public void receiveEvent(SaveQuestionEvent event) {
         log.info("Received event: {}", event);
         publisher.publishEvent(event);
     }
 
-    @RabbitListener(queues = "zzaug.review")
+    @RabbitHandler
     public void receiveEvent(DeleteQuestionEvent event) {
         log.info("Received event: {}", event);
         publisher.publishEvent(event);
