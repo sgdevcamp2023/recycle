eec70ec4245d36d767b42d1ffd5cf6eaedbcf7c2
sso9594 <sso9594@naver.com>
2024-02-13 14:07:07 +0900

    feat: application 이벤트를 받아 생성을 처리하는 usecase …
    Related to: #292

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQueryCreateUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQueryCreateUseCase.java
@@ -1,12 +1,14 @@
 package com.zzaug.review.domain.usecase.question.query;
 
 import com.zzaug.review.domain.dto.question.query.QuestionQueryCreateUseCaseRequest;
+import com.zzaug.review.domain.event.SaveQuestionEvent;
 import com.zzaug.review.domain.model.question.query.QuestionQuery;
 import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
 import com.zzaug.review.domain.support.entity.QuestionQueryEntityConverter;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -18,8 +20,9 @@ public class QuestionQueryCreateUseCase {
 	private final QuestionQueryConverter questionQueryConverter;
 
 	@Transactional
-	public void execute(QuestionQueryCreateUseCaseRequest request) {
-		QuestionQuery questionQuery = questionQueryConverter.from(request);
+	@EventListener
+	public void execute(SaveQuestionEvent event) {
+		QuestionQuery questionQuery = questionQueryConverter.from(event);
 		questionQueryRepository.save(QuestionQueryEntityConverter.from(questionQuery));
 	}
 }
