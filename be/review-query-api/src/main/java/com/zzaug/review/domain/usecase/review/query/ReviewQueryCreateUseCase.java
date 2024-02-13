0064697c7ab766b2f42ddf91e2015edce7689583
sso9594 <sso9594@naver.com>
2024-02-13 17:19:27 +0900

    feat: 리뷰 생성 이벤트를 전달받고 리뷰를 생성 및 질문글의 리뷰 수 카운트를 증가하는 로직 추가
    Related to: #293

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewQueryCreateUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewQueryCreateUseCase.java
@@ -1,13 +1,20 @@
 package com.zzaug.review.domain.usecase.review.query;
 
 import com.zzaug.review.domain.dto.review.query.ReviewQueryCreateUseCaseRequest;
+import com.zzaug.review.domain.event.SaveReviewEvent;
 import com.zzaug.review.domain.model.review.query.ReviewQuery;
+import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
 import com.zzaug.review.domain.persistence.review.ReviewQueryRepository;
 import com.zzaug.review.domain.support.entity.ReviewQueryEntityConverter;
 import javax.transaction.Transactional;
+
+import com.zzaug.review.entity.question.query.QuestionQueryEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 
 @Slf4j
 @Service
@@ -16,10 +23,17 @@ public class ReviewQueryCreateUseCase {
 
 	private final ReviewQueryRepository reviewQueryRepository;
 	private final ReviewQueryConverter reviewQueryConverter;
+	private final QuestionQueryRepository questionQueryRepository;
 
 	@Transactional
-	public void execute(ReviewQueryCreateUseCaseRequest request) {
-		ReviewQuery review = reviewQueryConverter.from(request);
+	@EventListener
+	public void execute(SaveReviewEvent event) {
+		QuestionQueryEntity parentQuestion = questionQueryRepository.findById(event.getQuestionId())
+				.orElseThrow(() -> new NoSuchElementException("Question not found"));
+		ReviewQuery review = reviewQueryConverter.from(event);
 		reviewQueryRepository.save(ReviewQueryEntityConverter.from(review));
+
+		parentQuestion.incReviewCnt();
+		questionQueryRepository.save(parentQuestion);
 	}
 }
