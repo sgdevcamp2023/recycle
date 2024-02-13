da757fde8f35ce69513938d32eb0c8d0150af332
sso9594 <sso9594@naver.com>
2024-02-13 17:44:52 +0900

    feat: rabbitmq에서 리뷰 요청 이벤트를 수신하여 어플리케이션 이벤트로 날리는 로직 추가
    Related to: #258

---
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/question/event/QuestionEventListener.java
@@ -0,0 +1,22 @@
+package com.zzaug.review.domain.usecase.question.event;
+
+import com.zzaug.review.domain.event.question.QuestionRequestEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class QuestionEventListener {
+    private final ApplicationEventPublisher publisher;
+
+    @RabbitListener(queues = "")
+    private void receiveEvent(QuestionRequestEvent event) {
+        log.debug("Event received: {}", event);
+        publisher.publishEvent(event);
+        log.debug("Event published: {}", event);
+    }
+}
