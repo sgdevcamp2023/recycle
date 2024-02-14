81444490b666a018b7d58c758427079968718d34
sso9594 <sso9594@naver.com>
2024-02-14 21:07:18 +0900

    refactor: transaction manager 명시 및 삭제 로직 변경
    Related to: #292

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQueryDeleteUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQueryDeleteUseCase.java
@@ -1,10 +1,9 @@
 package com.zzaug.review.domain.usecase.question.query;
 
-import com.zzaug.review.domain.dto.question.query.QuestionQueryDeleteUseCaseRequest;
-import com.zzaug.review.domain.event.DeleteQuestionEvent;
+import com.zzaug.review.config.JpaDataSourceConfig;
+import com.zzaug.review.domain.event.question.DeleteQuestionEvent;
 import com.zzaug.review.domain.model.question.query.QuestionQuery;
 import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
-import javax.transaction.Transactional;
 
 import com.zzaug.review.domain.support.entity.QuestionQueryEntityConverter;
 import com.zzaug.review.entity.question.query.QuestionQueryEntity;
@@ -12,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 
@@ -22,11 +22,13 @@ public class QuestionQueryDeleteUseCase {
 	private final QuestionQueryRepository questionQueryRepository;
 	private final QuestionQueryConverter questionQueryConverter;
 
-	@Transactional
+	@Transactional(JpaDataSourceConfig.TRANSACTION_MANAGER_NAME)
 	@EventListener
 	public void execute(DeleteQuestionEvent event) {
 		log.debug("Received event: {}", event);
-		QuestionQuery questionQuery = questionQueryConverter.from(event);
+		QuestionQueryEntity target = questionQueryRepository.findByQuestionId(event.getQuestionId())
+				.orElseThrow(() -> new NoSuchElementException("요청에 대한 응답을 찾을 수 없습니다."));
+		QuestionQuery questionQuery = questionQueryConverter.from(event, target);
 		questionQueryRepository.save(QuestionQueryEntityConverter.from(questionQuery));
 		log.debug("QuestionQuery deleted: {}", questionQuery);
 	}
