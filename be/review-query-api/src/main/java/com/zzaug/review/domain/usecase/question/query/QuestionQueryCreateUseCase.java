65a406a51d35c5483a7fd5ad98ca4ca98ef3b45d
sso9594 <sso9594@naver.com>
2024-02-14 21:06:53 +0900

    refactor: transaction manager 명시
    Related to: #292

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQueryCreateUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQueryCreateUseCase.java
@@ -1,15 +1,16 @@
 package com.zzaug.review.domain.usecase.question.query;
 
-import com.zzaug.review.domain.dto.question.query.QuestionQueryCreateUseCaseRequest;
-import com.zzaug.review.domain.event.SaveQuestionEvent;
+import com.zzaug.review.config.JpaDataSourceConfig;
+import com.zzaug.review.config.ReviewAppConfig;
+import com.zzaug.review.domain.event.question.SaveQuestionEvent;
 import com.zzaug.review.domain.model.question.query.QuestionQuery;
 import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
 import com.zzaug.review.domain.support.entity.QuestionQueryEntityConverter;
-import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -19,7 +20,7 @@ public class QuestionQueryCreateUseCase {
 	private final QuestionQueryRepository questionQueryRepository;
 	private final QuestionQueryConverter questionQueryConverter;
 
-	@Transactional
+	@Transactional(JpaDataSourceConfig.TRANSACTION_MANAGER_NAME)
 	@EventListener
 	public void execute(SaveQuestionEvent event) {
 		QuestionQuery questionQuery = questionQueryConverter.from(event);
