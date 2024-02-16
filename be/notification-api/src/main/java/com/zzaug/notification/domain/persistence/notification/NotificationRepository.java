3acf18a9798508ca21001f717276a24fb9aa0359
belljun3395 <195850@jnu.ac.kr>
2024-02-16 14:36:36 +0900

    remove: 삭제 필요하지 않은 파일

--- be/notification-api/src/main/java/com/zzaug/notification/domain/persistence/notification/NotificationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.zzaug.notification.domain.persistence.notification;
-
-import com.zzaug.notification.entity.notification.NotificationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {}
