ca9a6a4c62149d832c2b17a8caa2530ffed6704c
sso9594 <sso9594@naver.com>
2024-02-16 13:10:12 +0900

    refactor: 삭제 이벤트 변경에 따른 converter 수정
    Related to: #269

--- be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/event/converter/DeleteReviewEventConverter.java
+++ be/review-api/src/main/java/com/zzaug/review/domain/usecase/review/event/converter/DeleteReviewEventConverter.java
@@ -4,21 +4,18 @@ import com.zzaug.review.domain.event.review.DeleteReviewEvent;
 import com.zzaug.review.entity.review.ReviewEntity;
 import org.springframework.stereotype.Component;
 
+import java.sql.Timestamp;
+
 @Component
 public class DeleteReviewEventConverter {
 	public static DeleteReviewEvent from(ReviewEntity source) {
 		return DeleteReviewEvent.builder()
 				.reviewId(source.getReviewId())
 				.questionId(source.getQuestionId())
-				.content(source.getContent())
 				.author(source.getAuthor())
 				.authorId(source.getAuthorId())
-				.createdAt(source.getCreatedAt())
-				.updatedAt(source.getUpdatedAt())
-				.startPoint(source.getStartPoint())
-				.endPoint(source.getEndPoint())
-				.tag(source.getTag())
-				.isDeleted(true)
+				.deletedAt(source.getDeletedAt())
+				.eventAt(new Timestamp(System.currentTimeMillis()).toLocalDateTime())
 				.build();
 	}
 }
