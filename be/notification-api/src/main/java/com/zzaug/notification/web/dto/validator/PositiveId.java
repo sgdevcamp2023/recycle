c04e3d227738a5d326e156bfcea49431b696682d
belljun3395 <195850@jnu.ac.kr>
2024-02-16 14:40:18 +0900

    feat: 구현 PositiveId
    Related to: #325

---
+++ be/notification-api/src/main/java/com/zzaug/notification/web/dto/validator/PositiveId.java
@@ -0,0 +1,23 @@
+package com.zzaug.notification.web.dto.validator;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target({PARAMETER, FIELD})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = PositiveIdValidator.class)
+public @interface PositiveId {
+	String message() default "Id must be positive.";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
