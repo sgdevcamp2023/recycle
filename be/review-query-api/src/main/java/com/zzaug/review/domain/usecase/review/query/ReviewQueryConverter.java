1a0d25ccecd8623546188d0fbbd060c331c51607
sso9594 <sso9594@naver.com>
2024-02-16 14:25:06 +0900

    refactor: soft delete를 위한 속성 추가애 따른 변경사항 반영
    Related to: #293

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewQueryConverter.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewQueryConverter.java
@@ -1,10 +1,9 @@
 package com.zzaug.review.domain.usecase.review.query;
 
 import com.zzaug.review.domain.dto.review.query.ReviewQueryCreateUseCaseRequest;
-import com.zzaug.review.domain.dto.review.query.ReviewQueryEditUseCaseRequest;
-import com.zzaug.review.domain.event.DeleteReviewEvent;
-import com.zzaug.review.domain.event.EditReviewEvent;
-import com.zzaug.review.domain.event.SaveReviewEvent;
+import com.zzaug.review.domain.event.review.DeleteReviewEvent;
+import com.zzaug.review.domain.event.review.EditReviewEvent;
+import com.zzaug.review.domain.event.review.SaveReviewEvent;
 import com.zzaug.review.domain.model.review.query.ReviewQuery;
 import com.zzaug.review.entity.review.query.ReviewQueryEntity;
 import org.springframework.stereotype.Component;
@@ -35,40 +34,42 @@ public class ReviewQueryConverter {
 				.author(source.getAuthor())
 				.authorId(source.getAuthorId())
 				.createdAt(source.getCreatedAt())
-				.updatedAt(source.getUpdatedAt())
 				.startPoint(source.getStartPoint())
 				.endPoint(source.getEndPoint())
 				.tag(source.getTag())
+				.isDeleted(false)
 				.build();
 	}
 
-	public ReviewQuery from(EditReviewEvent source) {
+	public ReviewQuery from(EditReviewEvent source, ReviewQueryEntity reviewQueryEntity) {
 		return ReviewQuery.builder()
 				.reviewId(source.getReviewId())
 				.questionId(source.getQuestionId())
 				.content(source.getContent())
 				.author(source.getAuthor())
 				.authorId(source.getAuthorId())
-				.createdAt(source.getCreatedAt())
+				.createdAt(reviewQueryEntity.getCreatedAt())
 				.updatedAt(source.getUpdatedAt())
 				.startPoint(source.getStartPoint())
 				.endPoint(source.getEndPoint())
 				.tag(source.getTag())
+				.isDeleted(reviewQueryEntity.isDeleted())
 				.build();
 	}
 
-	public ReviewQuery from(DeleteReviewEvent source) {
+	public ReviewQuery from(DeleteReviewEvent source, ReviewQueryEntity reviewQueryEntity) {
 		return ReviewQuery.builder()
 				.reviewId(source.getReviewId())
 				.questionId(source.getQuestionId())
-				.content(source.getContent())
-				.author(source.getAuthor())
-				.authorId(source.getAuthorId())
-				.createdAt(source.getCreatedAt())
-				.updatedAt(source.getUpdatedAt())
-				.startPoint(source.getStartPoint())
-				.endPoint(source.getEndPoint())
-				.tag(source.getTag())
+				.content(reviewQueryEntity.getContent())
+				.author(reviewQueryEntity.getAuthor())
+				.authorId(reviewQueryEntity.getAuthorId())
+				.createdAt(reviewQueryEntity.getCreatedAt())
+				.updatedAt(reviewQueryEntity.getUpdatedAt())
+				.startPoint(reviewQueryEntity.getStartPoint())
+				.endPoint(reviewQueryEntity.getEndPoint())
+				.tag(reviewQueryEntity.getTag())
+				.isDeleted(true)
 				.build();
 	}
 }
