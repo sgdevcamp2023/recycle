d56cfed04ec3da29f49b0de011d795b986c1b721
belljun3395 <195850@jnu.ac.kr>
2024-02-16 14:44:59 +0900

    feat: 구현 RequestReviewUseCase
    Related to: #325

---
+++ be/notification-api/src/main/java/com/zzaug/notification/domain/usecase/notification/RequestReviewUseCase.java
@@ -0,0 +1,47 @@
+package com.zzaug.notification.domain.usecase.notification;
+
+import com.zzaug.notification.domain.dto.notification.RequestReviewUseCaseRequest;
+import com.zzaug.notification.domain.event.RequestReviewEvent;
+import com.zzaug.notification.domain.external.dao.RequestReviewHistoryDao;
+import com.zzaug.notification.entity.notification.RequestReviewHistoryEntity;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class RequestReviewUseCase {
+
+	private final RequestReviewHistoryDao requestReviewHistoryDao;
+	private final ApplicationEventPublisher applicationEventPublisher;
+
+	@Transactional
+	public void execute(RequestReviewUseCaseRequest request) {
+		final Long memberId = request.getMemberId();
+		final Long questionId = request.getQuestionId();
+		final Long requestMemberId = request.getRequestMemberId();
+
+		// todo bring memberInfo and check memberId is valid
+		// todo bring question info and check question owner is same with memberId
+		// todo bring memberInfo and check requestMemberId is valid
+		requestReviewHistoryDao.saveRequestReviewHistory(
+				RequestReviewHistoryEntity.builder()
+						.memberId(memberId)
+						.questionId(questionId)
+						.requestMemberId(requestMemberId)
+						.build());
+		publishEvent(questionId, memberId, requestMemberId);
+	}
+
+	private void publishEvent(Long questionId, Long memberId, Long requestMemberId) {
+		applicationEventPublisher.publishEvent(
+				RequestReviewEvent.builder()
+						.questionId(questionId)
+						.questionOwner(memberId)
+						.requestMemberId(requestMemberId)
+						.build());
+	}
+}
