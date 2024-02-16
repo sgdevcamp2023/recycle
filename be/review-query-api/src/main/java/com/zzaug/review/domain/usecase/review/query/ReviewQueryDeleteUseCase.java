4bc584359b35193f9a0eaf30bdc4f0ce92d61d3e
sso9594 <sso9594@naver.com>
2024-02-16 14:26:28 +0900

    refactor: transaction manager 명시 및 로직 변경 반영
    Related to: #293

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewQueryDeleteUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewQueryDeleteUseCase.java
@@ -1,18 +1,19 @@
 package com.zzaug.review.domain.usecase.review.query;
 
-import com.zzaug.review.domain.dto.review.query.ReviewQueryDeleteUseCaseRequest;
-import com.zzaug.review.domain.event.DeleteReviewEvent;
+import com.zzaug.review.config.JpaDataSourceConfig;
+import com.zzaug.review.domain.event.review.DeleteReviewEvent;
 import com.zzaug.review.domain.model.review.query.ReviewQuery;
 import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
 import com.zzaug.review.domain.persistence.review.ReviewQueryRepository;
-import javax.transaction.Transactional;
 
 import com.zzaug.review.domain.support.entity.ReviewQueryEntityConverter;
 import com.zzaug.review.entity.question.query.QuestionQueryEntity;
+import com.zzaug.review.entity.review.query.ReviewQueryEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 
@@ -24,12 +25,14 @@ public class ReviewQueryDeleteUseCase {
 	private final QuestionQueryRepository questionQueryRepository;
 	private final ReviewQueryConverter reviewQueryConverter;
 
-	@Transactional
+	@Transactional(JpaDataSourceConfig.TRANSACTION_MANAGER_NAME)
 	@EventListener
 	public void execute(DeleteReviewEvent event) {
 		QuestionQueryEntity parentQuestion = questionQueryRepository.findById(event.getQuestionId())
 				.orElseThrow(() -> new NoSuchElementException("Question not found"));
-		ReviewQuery review = reviewQueryConverter.from(event);
+		ReviewQueryEntity target = reviewQueryRepository.findById(event.getReviewId())
+				.orElseThrow(() -> new NoSuchElementException("Review not found"));
+		ReviewQuery review = reviewQueryConverter.from(event, target);
 		reviewQueryRepository.save(ReviewQueryEntityConverter.from(review));
 
 		parentQuestion.decReviewCnt();
