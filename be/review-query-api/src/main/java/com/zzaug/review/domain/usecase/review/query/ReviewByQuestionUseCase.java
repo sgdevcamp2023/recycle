4c3d0afccb0b3070e490cf665397f3f38e38c3d8
sso9594 <sso9594@naver.com>
2024-02-18 23:51:25 +0900

    refactor: transaction manager 명시
    Related to: #338

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewByQuestionUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/review/query/ReviewByQuestionUseCase.java
@@ -1,15 +1,17 @@
 package com.zzaug.review.domain.usecase.review.query;
 
+import com.zzaug.review.config.JpaDataSourceConfig;
 import com.zzaug.review.domain.dto.review.query.ReviewByQuestionUseCaseRequest;
 import com.zzaug.review.domain.dto.review.query.ReviewQueryResponse;
 import com.zzaug.review.domain.persistence.review.ReviewQueryRepository;
 import com.zzaug.review.entity.review.query.ReviewQueryEntity;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -17,7 +19,7 @@ import org.springframework.stereotype.Service;
 public class ReviewByQuestionUseCase {
 	private final ReviewQueryRepository reviewQueryRepository;
 
-	@Transactional
+	@Transactional(JpaDataSourceConfig.TRANSACTION_MANAGER_NAME)
 	public List<ReviewQueryResponse> execute(ReviewByQuestionUseCaseRequest request) {
 		List<ReviewQueryEntity> result =
 				reviewQueryRepository.findAllByQuestionIdAndIsDeletedIsFalse(request.getQuestionId());
