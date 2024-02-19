c08b3240ec5869c1e83b8d7d36bfdfbed5fb6441
sso9594 <sso9594@naver.com>
2024-02-19 13:35:19 +0900

    refactor: transaction manager 명시
    Related to: #339

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQueryViewUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQueryViewUseCase.java
@@ -1,14 +1,16 @@
 package com.zzaug.review.domain.usecase.question.query;
 
+import com.zzaug.review.config.JpaDataSourceConfig;
 import com.zzaug.review.domain.dto.question.query.QuestionQueryResponse;
 import com.zzaug.review.domain.dto.question.query.QuestionQueryViewUseCaseRequest;
 import com.zzaug.review.domain.model.question.query.QuestionQuery;
 import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
 import java.util.NoSuchElementException;
-import javax.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -19,7 +21,7 @@ public class QuestionQueryViewUseCase {
 	private final QuestionQueryConverter questionQueryConverter;
 	private final QuestionQueryResponseConverter questionQueryResponseConverter;
 
-	@Transactional
+	@Transactional(JpaDataSourceConfig.TRANSACTION_MANAGER_NAME)
 	public QuestionQueryResponse execute(QuestionQueryViewUseCaseRequest request) {
 		QuestionQuery result =
 				questionQueryConverter.from(
