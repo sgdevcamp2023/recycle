c6b23366727203e19ffc6f64edb3baf8667a648f
sso9594 <sso9594@naver.com>
2024-02-19 13:34:56 +0900

    feat: 문장 단위의 검색을 위한 repository 변경
    Related to: #339

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQuerySearchUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/QuestionQuerySearchUseCase.java
@@ -1,25 +1,26 @@
 package com.zzaug.review.domain.usecase.question.query;
 
+import com.zzaug.review.config.JpaDataSourceConfig;
 import com.zzaug.review.domain.dto.question.query.QuestionQueryResponse;
 import com.zzaug.review.domain.dto.question.query.QuestionQuerySearchUseCaseRequest;
-import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
+import com.zzaug.review.domain.persistence.question.QuestionSearchQueryRepository;
 import com.zzaug.review.entity.question.query.QuestionQueryEntity;
-import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class QuestionQuerySearchUseCase {
 
-	private final QuestionQueryRepository questionQueryRepository;
+	private final QuestionSearchQueryRepository questionQueryRepository;
 	private final QuestionQueryConverter questionQueryConverter;
 	private final QuestionQueryResponseConverter questionQueryResponseConverter;
 
-	@Transactional
+	@Transactional(JpaDataSourceConfig.TRANSACTION_MANAGER_NAME)
 	public Page<QuestionQueryResponse> execute(QuestionQuerySearchUseCaseRequest request) {
 		Page<QuestionQueryEntity> results =
 				questionQueryRepository.findAllByAuthorIdAndContentContainingAndIsDeletedIsFalse(
