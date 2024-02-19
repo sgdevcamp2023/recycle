bc0433ee0602725c0bfe25608a2e0b9e4eb52c91
sso9594 <sso9594@naver.com>
2024-02-19 13:35:34 +0900

    feat: 질문 글 검색 메소드 추가
    Related to: #339

--- be/review-query-api/src/main/java/com/zzaug/review/domain/persistence/question/QuestionSearchQueryRepository.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/persistence/question/QuestionSearchQueryRepository.java
@@ -3,8 +3,13 @@ package com.zzaug.review.domain.persistence.question;
 import com.zzaug.review.entity.question.query.QuestionQueryEntity;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHitSupport;
+import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
@@ -34,5 +39,20 @@ public class QuestionSearchQueryRepository {
         } else {
             return null;
         }
+    }
+
+    public Page<QuestionQueryEntity> findAllByAuthorIdAndContentContainingAndIsDeletedIsFalse(
+            Pageable pageable, Long authorId, String query) {
+        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.boolQuery()
+                        .must(QueryBuilders.matchQuery("authorId", authorId))
+                        .must(QueryBuilders.matchQuery("content", query))
+                        .must(QueryBuilders.matchQuery("isDeleted", false)))
+                .withPageable(pageable)
+                .build();
+        SearchHits<QuestionQueryEntity> searchHits = elasticsearchOperations
+                .search(searchQuery, QuestionQueryEntity.class, IndexCoordinates.of("question"));
+
+        return SearchHitSupport.searchPageFor(searchHits, searchQuery.getPageable()).map(SearchHit::getContent);
     }
 }
