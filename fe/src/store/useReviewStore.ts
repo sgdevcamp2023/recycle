bdff8252e17b754158933d1ae55b3d074e6f4b9f
krokerdile <crazykroker@gmail.com>
2024-02-19 14:44:35 +0900

    feat: reviewStore 내용 추가 - reviewList, setReviewList 추가 Related to LineComment, CreateReview에 적용 #340

--- fe/src/store/useReviewStore.ts
+++ fe/src/store/useReviewStore.ts
@@ -3,8 +3,10 @@ import { create } from 'zustand';
 const useReviewStore = create((set) => ({
   id: '',
   review: '',
+  reviewList: [],
   setId: (newId) => set({ id: newId }),
   setReview: (newReview) => set({ review: newReview }),
+  setReviewList: (reviewList) => set({ reviewList: reviewList }),
 }));
 
 export default useReviewStore;
