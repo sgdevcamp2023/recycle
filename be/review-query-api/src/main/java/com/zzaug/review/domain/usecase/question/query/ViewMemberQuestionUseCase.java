83f5fd1efde11929029a6b6432d57203fa2c3b76
sso9594 <sso9594@naver.com>
2024-02-20 15:08:01 +0900

    refactor: local setting을 위한 파일 수정
    Related to: #352

--- be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/ViewMemberQuestionUseCase.java
+++ be/review-query-api/src/main/java/com/zzaug/review/domain/usecase/question/query/ViewMemberQuestionUseCase.java
@@ -1,15 +1,17 @@
 package com.zzaug.review.domain.usecase.question.query;
 
+import com.zzaug.review.config.JpaDataSourceConfig;
 import com.zzaug.review.domain.dto.question.query.QuestionQueryResponse;
 import com.zzaug.review.domain.dto.review.query.ViewMemberQuestionUseCaseRequest;
 import com.zzaug.review.domain.persistence.question.QuestionQueryRepository;
 import com.zzaug.review.entity.question.query.QuestionQueryEntity;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -17,7 +19,7 @@ import org.springframework.stereotype.Service;
 public class ViewMemberQuestionUseCase {
 	private final QuestionQueryRepository questionQueryRepository;
 
-	@Transactional
+	@Transactional(JpaDataSourceConfig.TRANSACTION_MANAGER_NAME)
 	public List<QuestionQueryResponse> execute(ViewMemberQuestionUseCaseRequest request) {
 		List<QuestionQueryEntity> result =
 				questionQueryRepository.findAllByAuthorIdAndIsDeletedIsFalse(request.getAuthorId());
