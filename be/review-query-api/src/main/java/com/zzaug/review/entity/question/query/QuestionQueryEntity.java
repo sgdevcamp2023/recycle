5e7b635518aba3667f054e5ed110d88144f924c4
sso9594 <sso9594@naver.com>
2024-02-22 00:48:12 +0900

    refactor: local setting을 위한 파일 수정
    Related to: #352

--- be/review-query-api/src/main/java/com/zzaug/review/entity/question/query/QuestionQueryEntity.java
+++ be/review-query-api/src/main/java/com/zzaug/review/entity/question/query/QuestionQueryEntity.java
@@ -1,6 +1,12 @@
 package com.zzaug.review.entity.question.query;
 
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.Id;
@@ -32,6 +38,9 @@ public class QuestionQueryEntity {
 			type = FieldType.Date,
 			format = DateFormat.custom,
 			pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS||epoch_millis")
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime createdAt;
 
 	@Field(
